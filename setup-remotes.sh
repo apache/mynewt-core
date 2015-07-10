85c98ef389486d08cf8692364d78d73c5b2f69f1
sterling <sterling@micosa.net>
2015-07-10 14:54:48 -0700

    add a tool to setup remotes for subtree merging

---
+++ setup-remotes.sh
@@ -0,0 +1,6 @@
+git remote add os https://github.com/micosa/os.git
+git remote add bootutil https://github.com/micosa/bootutil.git 
+git remote add cmsis-core https://github.com/micosa/cmsis-core.git 
+git remote add boot_project https://github.com/micosa/boot_project.git
+git remote add olimex_stm32-e407_devboard https://github.com/micosa/olimex_stm32-e407_devboard.git 
+git remote add olimex_stm32-e407_devboard-boot https://github.com/micosa/olimex_stm32-e407_devboard-boot.git 
