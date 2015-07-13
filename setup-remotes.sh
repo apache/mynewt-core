6e8da0518e4846a649550a1e8da6317935408913
sterling <sterling@micosa.net>
2015-07-13 16:55:40 -0700

    add sim_compiler remote for subtrees

--- setup-remotes.sh
+++ setup-remotes.sh
@@ -10,3 +10,5 @@ git remote add boot_project https://github.com/micosa/boot_project.git
 git remote add olimex_stm32-e407_devboard https://github.com/micosa/olimex_stm32-e407_devboard.git 
 # subtree @ hw/bsp/olimex_stm32-e407_devboard-boot 
 git remote add olimex_stm32-e407_devboard-boot https://github.com/micosa/olimex_stm32-e407_devboard-boot.git 
+# subtree @ compiler/sim 
+git remote add compiler_sim https://github.com/micosa/sim_compiler.git
