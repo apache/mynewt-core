8d12f98c1e0a95ea395e48279c5dacf390bf563a
Willam San Filippo <will@micosa.io>
2015-07-24 13:51:48 -0700

    Added some gpio test code to test interrupts

--- project/main/src/main.c
+++ project/main/src/main.c
@@ -5,7 +5,6 @@
 ** $Id: main.c 76842 2015-03-26 05:00:46Z wills $
 ******************************************************************************/
 #include "os/os.h"
-/* WWW: This is a hack! I should not have to define the path in this manner. */
 #include "hal/hal_gpio.h"
 #include <assert.h>
 
@@ -13,13 +12,75 @@
 volatile int tasks_initialized;
 int init_tasks(void);
 
-static volatile int g_task1_loops;
-
+/* Task 1 */
 #define TASK1_PRIO (1) 
 struct os_task task1;
 os_stack_t stack1[OS_STACK_ALIGN(1024)];
+static volatile int g_task1_loops;
 
-int led_pin;
+/* Task 2 */
+#define TASK2_PRIO (2) 
+struct os_task task2;
+os_stack_t stack2[OS_STACK_ALIGN(1024)];
+static volatile int g_task2_loops;
+
+/* Global test semaphore */
+struct os_sem g_test_sem;
+
+/* For LED toggling */
+int g_led_pin;
+
+/* For GPIO test code */
+int g_gpio_test_out_pin;
+int g_gpio_test_in_pin;
+uint32_t g_gpio_test_irqs;
+
+void
+gpio_test_irq(void *arg)
+{
+    if (arg == NULL) {
+        assert(0);
+    }
+    ++g_gpio_test_irqs;
+
+    if (g_gpio_test_irqs > 1) {
+        os_sem_release(&g_test_sem);
+    }
+}
+
+void gpio_test(void)
+{
+    int rc;
+
+    /* Set gpio test pins */
+    g_gpio_test_out_pin = 34;
+    gpio_init_out(g_gpio_test_out_pin, 0);
+
+    /* Set up the gpio test input pin */
+    g_gpio_test_in_pin = 26;
+    rc = gpio_irq_init(g_gpio_test_in_pin, gpio_test_irq, &g_gpio_test_irqs,
+                       GPIO_TRIG_RISING, GPIO_PULL_DOWN);
+    assert(rc == 0);
+
+    /* Make sure no interrupts yet */
+    assert(g_gpio_test_irqs == 0);
+
+    /* Read test input pin. Should read low */
+    rc = gpio_read(g_gpio_test_in_pin);
+    assert(rc == 0);
+
+    /* Set output high */
+    gpio_set(g_gpio_test_out_pin);
+
+    /* Make sure only one interrupt occurred */
+    assert(g_gpio_test_irqs == 1);
+
+    /* Set output low */
+    gpio_clear(g_gpio_test_out_pin);
+
+    /* Make sure no interrupt occurred */
+    assert(g_gpio_test_irqs == 1);
+}
 
 void 
 task1_handler(void *arg)
@@ -27,8 +88,11 @@ task1_handler(void *arg)
     struct os_task *t;
 
     /* Set the led pin for the E407 devboard */
-    led_pin = 45;
-    gpio_init_out(led_pin, 1);
+    g_led_pin = 45;
+    gpio_init_out(g_led_pin, 1);
+
+    /* Test gpio */
+    gpio_test();
 
     while (1) {
         t = os_sched_get_current_task();
@@ -37,7 +101,29 @@ task1_handler(void *arg)
         ++g_task1_loops;
 
         os_time_delay(1000);
-        gpio_toggle(led_pin);
+
+        /* Toggle the LED */
+        gpio_toggle(g_led_pin);
+
+        /* Toggle the test gpio to create an interrupt (every other toggle) */
+        gpio_toggle(g_gpio_test_out_pin);
+    }
+}
+
+void 
+task2_handler(void *arg)
+{
+    struct os_task *t;
+
+    while (1) {
+        t = os_sched_get_current_task();
+        assert(t->t_func == task2_handler);
+
+        /* Wait for semaphore from ISR */
+        os_sem_pend(&g_test_sem, OS_TIMEOUT_NEVER);
+
+        /* Increment # of times we went through task loop */
+        ++g_task2_loops;
     }
 }
 
@@ -54,8 +140,14 @@ task1_handler(void *arg)
 int
 init_tasks(void)
 {
+    /* Create global test semaphore */
+    os_sem_create(&g_test_sem, 0);
+
     os_task_init(&task1, "task1", task1_handler, NULL, 
             TASK1_PRIO, stack1, OS_STACK_ALIGN(1024));
+
+    os_task_init(&task2, "task2", task2_handler, NULL, 
+            TASK2_PRIO, stack2, OS_STACK_ALIGN(1024));
     tasks_initialized = 1;
     return 0;
 }
