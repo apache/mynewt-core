16e1f21cf64472db2ec6adf524e70f250c233fb6
sterling <sterling@micosa.net>
2015-08-05 10:26:14 -0700

    change comment to reflect libs/os not pkg/os

--- setup-remotes.sh
+++ setup-remotes.sh
@@ -1,8 +1,8 @@
-# subtree @ pkg/os
+# subtree @ libs/os
 git remote add os https://github.com/micosa/os.git
-# subtree @ pkg/bootutil
+# subtree @ libs/bootutil
 git remote add bootutil https://github.com/micosa/bootutil.git 
-# subtree @ pkg/cmsis-core 
+# subtree @ libs/cmsis-core 
 git remote add cmsis-core https://github.com/micosa/cmsis-core.git 
 # subtree @ project/boot 
 git remote add boot_project https://github.com/micosa/boot_project.git
