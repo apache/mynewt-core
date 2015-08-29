b87709f78f007755125c446eaa95d7f2dd85b8e5
Christopher Collins <ccollins476ad@gmail.com>
2015-08-28 19:06:26 -0700

    Remove boot bsp repo from setup-remotes.sh

--- setup-remotes.sh
+++ setup-remotes.sh
@@ -8,8 +8,6 @@ git remote add cmsis-core https://github.com/micosa/cmsis-core.git
 git remote add boot_project https://github.com/micosa/boot_project.git
 # subtree @ hw/bsp/olimex_stm32-e407_devboard 
 git remote add olimex_stm32-e407_devboard https://github.com/micosa/olimex_stm32-e407_devboard.git 
-# subtree @ hw/bsp/olimex_stm32-e407_devboard-boot 
-git remote add olimex_stm32-e407_devboard-boot https://github.com/micosa/olimex_stm32-e407_devboard-boot.git 
 # subtree @ compiler/sim 
 git remote add compiler_sim https://github.com/micosa/sim_compiler.git
 # subtree @ hw/mcu
