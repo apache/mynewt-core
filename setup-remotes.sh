dd838ebc2f538a024701a7769d3c0605025aada0
sterling <sterling@micosa.net>
2015-09-14 18:03:05 -0700

    change micosa to stackos

--- setup-remotes.sh
+++ setup-remotes.sh
@@ -1,26 +1,26 @@
 # subtree @ libs/os
-git remote add os https://github.com/micosa/os.git
+git remote add os https://github.com/stackos/os.git
 # subtree @ libs/bootutil
-git remote add bootutil https://github.com/micosa/bootutil.git 
+git remote add bootutil https://github.com/stackos/bootutil.git 
 # subtree @ libs/cmsis-core 
-git remote add cmsis-core https://github.com/micosa/cmsis-core.git 
+git remote add cmsis-core https://github.com/stackos/cmsis-core.git 
 # subtree @ project/boot 
-git remote add boot_project https://github.com/micosa/boot_project.git
+git remote add boot_project https://github.com/stackos/boot_project.git
 # subtree @ hw/bsp/olimex_stm32-e407_devboard 
-git remote add olimex_stm32-e407_devboard https://github.com/micosa/olimex_stm32-e407_devboard.git 
+git remote add olimex_stm32-e407_devboard https://github.com/stackos/olimex_stm32-e407_devboard.git 
 # subtree @ compiler/sim 
-git remote add compiler_sim https://github.com/micosa/sim_compiler.git
+git remote add compiler_sim https://github.com/stackos/sim_compiler.git
 # subtree @ hw/mcu
-git remote add mcu https://github.com/micosa/mcu.git
+git remote add mcu https://github.com/stackos/mcu.git
 # subtree @ hw/hal
-git remote add hal https://github.com/micosa/hal.git
+git remote add hal https://github.com/stackos/hal.git
 # subtree @ libs/ffs
-git remote add ffs https://github.com/micosa/ffs.git
+git remote add ffs https://github.com/stackos/ffs.git
 # subtree @ libs/ffsutil
-git remote add ffsutil https://github.com/micosa/ffsutil.git
+git remote add ffsutil https://github.com/stackos/ffsutil.git
 # subtree @ project/bin2img
-git remote add bin2img https://github.com/micosa/bin2img.git
+git remote add bin2img https://github.com/stackos/bin2img.git
 # subtree @ libs/testutil
-git remote add testutil https://github.com/micosa/testutil.git
+git remote add testutil https://github.com/stackos/testutil.git
 # subtree @ hw/bsp/native
-git remote add native-bsp https://github.com/micosa/native-bsp.git
+git remote add native-bsp https://github.com/stackos/native-bsp.git
