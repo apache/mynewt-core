00834a94d486d288eec0eaa2902e92207a22ecf0
sterling <sterling@micosa.net>
2015-09-21 15:00:08 -0700

    add m_append() equivalent.  document mbuf code now that structure is decided.  update test cases.

--- libs/os/include/os/os_mbuf.h
+++ libs/os/include/os/os_mbuf.h
@@ -17,29 +17,195 @@
 #ifndef _OS_MBUF_H 
 #define _OS_MBUF_H 
 
+
+/**
+ * A mbuf pool to allocate a mbufs out of.  This contains a pointer to the 
+ * mempool to allocate mbufs out of, along with convenient housekeeping 
+ * information on mbufs in the pool (e.g. length of variable packet header)
+ */
 struct os_mbuf_pool {
-    uint16_t omp_hdr_size;
-    uint16_t omp_buf_size;
-    uint16_t omp_buf_count;
+    /** 
+     * Total length of the databuf in each mbuf.  This is the size of the 
+     * mempool block, minus the mbuf header
+     */
+    uint16_t omp_databuf_len;
+    /**
+     * Total number of memblock's allocated in this mempool.
+     */
+    uint16_t omp_mbuf_count;
+    /**
+     * The length of the variable portion of the mbuf header
+     */
+    uint16_t omp_hdr_len;
+    /**
+     * The memory pool which to allocate mbufs out of 
+     */
     struct os_mempool *omp_pool;
 };
 
+
+/**
+ * A packet header structure that preceeds the mbuf packet headers.
+ */
+struct os_mbuf_pkthdr {
+    /**
+     * Overall length of the packet. 
+     */
+    uint32_t omp_len;
+    /**
+     * Next packet in the mbuf chain.
+     */
+    STAILQ_ENTRY(os_mbuf_pkthdr) omp_next;
+};
+
+/**
+ * Chained memory buffer.
+ */
 struct os_mbuf {
+    /**
+     * Current pointer to data in the structure
+     */
+    uint8_t *om_data;
+    /**
+     * Flags associated with this buffer, see OS_MBUF_F_* defintions
+     */
     uint16_t om_flags;
+    /**
+     * Length of data in this buffer 
+     */
     uint16_t om_len;
 
+    /**
+     * Pointer to next entry in the chained memory buffer
+     */
     SLIST_ENTRY(os_mbuf) om_next;
 
-    uint8_t om_data[0];
+    /**
+     * Pointer to the beginning of the data, after this buffer
+     */
+    uint8_t om_databuf[0];
 };
+
+/*
+ * Mbuf flags: 
+ *  - OS_MBUF_F_PKTHDR: Whether or not this mbuf is a packet header mbuf
+ *  - OS_MBUF_F_USER: The base user defined mbuf flag, start defining your 
+ *    own flags from this flag number.
+ */
+
+#define OS_MBUF_F_PKTHDR (0)
+#define OS_MBUF_F_USER   (OS_MBUF_F_PKTHDR + 1) 
+
+/*
+ * Given a flag number, provide the mask for it
+ *
+ * @param __n The number of the flag in the mask 
+ */
+#define OS_MBUF_F_MASK(__n) (1 << (__n))
+
+/* 
+ * Checks whether a given mbuf is a packet header mbuf 
+ *
+ * @param __om The mbuf to check 
+ */
+#define OS_MBUF_IS_PKTHDR(__om) \
+    ((__om)->om_flags & OS_MBUF_F_MASK(OS_MBUF_F_PKTHDR))
+
+
+#define OS_MBUF_PKTHDR(__om) ((struct os_mbuf_pkthdr *) &(__om)->om_databuf[0])
+
+/*
+ * Access the data of a mbuf, and cast it to type
+ *
+ * @param __om The mbuf to access, and cast 
+ * @param __type The type to cast it to 
+ */
+#define OS_MBUF_DATA(__om, __type) \
+     (__type) ((__om)->om_data)
+
+
+
+
+/**
+ * Returns the end offset of a mbuf buffer 
+ * 
+ * @param __omp 
+ */
+#define OS_MBUF_END_OFF(__omp) ((__omp)->omp_databuf_len)
+
+/**
+ * Returns the start offset of a mbuf buffer
+ */
+#define OS_MBUF_START_OFF(__omp) (0)
+
+
+/*
+ * Called by OS_MBUF_LEADINGSPACE() macro
+ */
+static inline uint16_t 
+_os_mbuf_leadingspace(struct os_mbuf_pool *omp, struct os_mbuf *om)
+{
+    uint16_t startoff;
+    uint16_t leadingspace;
+
+    startoff = 0;
+    if (OS_MBUF_IS_PKTHDR(om)) {
+        startoff = sizeof(struct os_mbuf_pkthdr) + omp->omp_hdr_len;
+    }
+
+    leadingspace = (uint16_t) (OS_MBUF_DATA(om, uint8_t *) - 
+        ((uint8_t *) &om->om_databuf[0] + startoff));
+
+    return (leadingspace);
+}
+
+/**
+ * Returns the leading space (space at the beginning) of the mbuf. 
+ * Works on both packet header, and regular mbufs, as it accounts 
+ * for the additional space allocated to the packet header.
+ * 
+ * @param __omp Is the mbuf pool (which contains packet header length.)
+ * @param __om  Is the mbuf in that pool to get the leadingspace for 
+ *
+ * @return Amount of leading space available in the mbuf 
+ */
+#define OS_MBUF_LEADINGSPACE(__omp, __om) _os_mbuf_leadingspace(__omp, __om)
+
+/* Called by OS_MBUF_TRAILINGSPACE() macro. */
+static inline uint16_t 
+_os_mbuf_trailingspace(struct os_mbuf_pool *omp, struct os_mbuf *om)
+{
+    return (&om->om_databuf[0] + omp->omp_databuf_len) - om->om_data;
+}
+
+/**
+ * Returns the trailing space (space at the end) of the mbuf.
+ * Works on both packet header and regular mbufs.
+ *
+ * @param __omp The mbuf pool for this mbuf 
+ * @param __om  Is the mbuf in that pool to get trailing space for 
+ *
+ * @return The amount of trailing space available in the mbuf 
+ */
+#define OS_MBUF_TRAILINGSPACE(__omp, __om) _os_mbuf_trailingspace(__omp, __om)
+
 
 /* Initialize a mbuf pool */
 int os_mbuf_pool_init(struct os_mbuf_pool *, struct os_mempool *mp, uint16_t, 
         uint16_t, uint16_t);
 
 /* Allocate a new mbuf out of the os_mbuf_pool */ 
-struct os_mbuf *os_mbuf_get(struct os_mbuf_pool *omp, uint16_t data_start);
+struct os_mbuf *os_mbuf_get(struct os_mbuf_pool *omp, uint16_t);
+/* Duplicate a mbuf from the pool */
+struct os_mbuf *os_mbuf_dup(struct os_mbuf_pool *omp, struct os_mbuf *m);
+
+/* Append data onto a mbuf */
+int os_mbuf_append(struct os_mbuf_pool *omp, struct os_mbuf *m, void *, 
+        uint16_t);
+
 /* Free a mbuf */
 int os_mbuf_free(struct os_mbuf_pool *omp, struct os_mbuf *mb);
+/* Free a mbuf chain */
+int os_mbuf_free_chain(struct os_mbuf_pool *omp, struct os_mbuf *om);
 
 #endif /* _OS_MBUF_H */ 
--- libs/os/src/test/mbuf_test.c
+++ libs/os/src/test/mbuf_test.c
@@ -18,35 +18,114 @@
 #include "os/os.h"
 #include "os_test_priv.h"
 
+#include <string.h>
+
 #define MBUF_TEST_POOL_BUF_SIZE (256)
 #define MBUF_TEST_POOL_BUF_COUNT (10) 
 
-os_membuf_t mbuf_mempool[OS_MEMPOOL_SIZE(MBUF_TEST_POOL_BUF_SIZE, 
+os_membuf_t os_mbuf_membuf[OS_MEMPOOL_SIZE(MBUF_TEST_POOL_BUF_SIZE, 
         MBUF_TEST_POOL_BUF_COUNT)];
 
-TEST_CASE(os_mbuf_test_case_1)
+struct os_mbuf_pool os_mbuf_pool; 
+struct os_mempool os_mbuf_mempool; 
+
+static void 
+os_mbuf_test_setup(void)
 {
-    struct os_mbuf_pool omp; 
-    struct os_mempool mp;
-    struct os_mbuf *m; 
     int rc;
 
-    rc = os_mempool_init(&mp, MBUF_TEST_POOL_BUF_COUNT, 
-            MBUF_TEST_POOL_BUF_SIZE, &mbuf_mempool[0], "mbuf_pool");
+    rc = os_mempool_init(&os_mbuf_mempool, MBUF_TEST_POOL_BUF_COUNT, 
+            MBUF_TEST_POOL_BUF_SIZE, &os_mbuf_membuf[0], "mbuf_pool");
     TEST_ASSERT_FATAL(rc == 0, "Error creating memory pool %d", rc);
 
-    rc = os_mbuf_pool_init(&omp, &mp, 0, MBUF_TEST_POOL_BUF_SIZE, MBUF_TEST_POOL_BUF_COUNT); 
+    rc = os_mbuf_pool_init(&os_mbuf_pool, &os_mbuf_mempool, 0, 
+            MBUF_TEST_POOL_BUF_SIZE, MBUF_TEST_POOL_BUF_COUNT);
     TEST_ASSERT_FATAL(rc == 0, "Error creating mbuf pool %d", rc);
-
-    m = os_mbuf_get(&omp, 0);
-    TEST_ASSERT_FATAL(m != NULL, "Error allocating mbuf");
-
-    rc = os_mbuf_free(&omp, m);
-    TEST_ASSERT_FATAL(rc == 0, "Error free'ing mbuf %d", rc);
 }
 
 
+
+TEST_CASE(os_mbuf_test_case_1)
+{
+    struct os_mbuf *m; 
+    int rc;
+
+    m = os_mbuf_get(&os_mbuf_pool, 0);
+    TEST_ASSERT_FATAL(m != NULL, "Error allocating mbuf");
+
+    rc = os_mbuf_free(&os_mbuf_pool, m);
+    TEST_ASSERT_FATAL(rc == 0, "Error free'ing mbuf %d", rc);
+}
+
+TEST_CASE(os_mbuf_test_case_2) 
+{
+    struct os_mbuf *m;
+    struct os_mbuf *m2;
+    struct os_mbuf *dup;
+    int rc;
+
+    /* Test first allocating and duplicating a single mbuf */
+    m = os_mbuf_get(&os_mbuf_pool, 0);
+    TEST_ASSERT_FATAL(m != NULL, "Error allocating mbuf");
+
+    dup = os_mbuf_dup(&os_mbuf_pool, m);
+    TEST_ASSERT_FATAL(dup != NULL, "NULL mbuf returned from dup");
+    TEST_ASSERT_FATAL(dup != m, "duplicate matches original.");
+
+    rc = os_mbuf_free(&os_mbuf_pool, m);
+    TEST_ASSERT_FATAL(rc == 0, "Error free'ing mbuf m %d", rc);
+    
+    rc = os_mbuf_free(&os_mbuf_pool, dup);
+    TEST_ASSERT_FATAL(rc == 0, "Error free'ing mbuf dup %d", rc);
+
+    m = os_mbuf_get(&os_mbuf_pool, 0);
+    TEST_ASSERT_FATAL(m != NULL, "Error allocating mbuf"); 
+
+    m2 = os_mbuf_get(&os_mbuf_pool, 0);
+    TEST_ASSERT_FATAL(m2 != NULL, "Error allocating mbuf");
+
+    SLIST_NEXT(m, om_next) = m2; 
+
+    dup = os_mbuf_dup(&os_mbuf_pool, m);
+    TEST_ASSERT_FATAL(dup != NULL, "NULL mbuf returned from dup");
+    TEST_ASSERT_FATAL(dup != m, "Duplicate matches original");
+    TEST_ASSERT_FATAL(SLIST_NEXT(dup, om_next) != NULL, 
+            "NULL chained element, duplicate should match original");
+
+    rc = os_mbuf_free_chain(&os_mbuf_pool, m);
+    TEST_ASSERT_FATAL(rc == 0, "Cannot free mbuf chain %d", rc);
+
+    rc = os_mbuf_free_chain(&os_mbuf_pool, dup);
+    TEST_ASSERT_FATAL(rc == 0, "Cannot free mbuf chain %d", rc);
+}
+
+TEST_CASE(os_mbuf_test_case_3) 
+{
+    struct os_mbuf *m; 
+    int rc;
+    uint8_t databuf[] = {0xa, 0xb, 0xc, 0xd};
+    uint8_t cmpbuf[] = {0xff, 0xff, 0xff, 0xff};
+
+    m = os_mbuf_get(&os_mbuf_pool, 0);
+    TEST_ASSERT_FATAL(m != NULL, "Error allocating mbuf");
+
+    rc = os_mbuf_append(&os_mbuf_pool, m, databuf, sizeof(databuf));
+    TEST_ASSERT_FATAL(rc == 0, "Cannot add %d bytes to mbuf", 
+            sizeof(databuf));
+    TEST_ASSERT_FATAL(m->om_len == sizeof(databuf), 
+            "Length doesn't match size appended %d vs %d", m->om_len, 
+            sizeof(databuf));
+
+    memcpy(cmpbuf, OS_MBUF_DATA(m, uint8_t *), m->om_len);
+    TEST_ASSERT_FATAL(memcmp(cmpbuf, databuf, sizeof(databuf)) == 0, 
+            "Databuf doesn't match cmpbuf");
+}
+
 TEST_SUITE(os_mbuf_test_suite)
 {
+    os_mbuf_test_setup();
+
     os_mbuf_test_case_1();
+    os_mbuf_test_case_2();
+    os_mbuf_test_case_3();
 }
