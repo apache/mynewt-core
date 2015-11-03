6a7a34dfbf3f4207f1eb872d8c8fcb9a77c99408
Marko Kiiskila <marko@runtime.io>
2015-11-03 14:38:03 -0800

    Add download/debug support files for Olimex E407 BSP.

---
+++ hw/bsp/olimex_stm32-e407_devboard/olimex_stm32-e407_devboard_download.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Called: $0 <binary> [identities...]
+#  - binary is the path to prefix to target binary, .elf.bin appended to this
+#    name is the raw binary format of the binary.
+#  - identities is the project identities string. So you can have e.g. different
+#    flash offset for bootloader identity
+# 
+#
+if [ $# -lt 1 ]; then
+    echo "Need binary to download"
+    exit 1
+fi
+
+FLASH_OFFSET=0x08000000
+FILE_NAME=$1.elf.bin
+
+echo "Downloading" $FILE_NAME
+
+openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -s hw/bsp/olimex_stm32-e407_devboard -f f407.cfg -c init -c "reset halt" -c "flash write_image erase $FILE_NAME $FLASH_OFFSET" -c "reset run" -c shutdown
+
