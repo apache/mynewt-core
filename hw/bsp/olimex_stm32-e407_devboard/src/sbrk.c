73101a1982964dea76398012b6c0e4734ea623f4
Marko Kiiskila <marko@runtime.io>
2015-11-24 11:13:20 -0800

    Switch to use baselibc for bootloader.

--- hw/bsp/olimex_stm32-e407_devboard/src/sbrk.c
+++ hw/bsp/olimex_stm32-e407_devboard/src/sbrk.c
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-#include <errno.h>
 
 extern char __HeapBase;
 extern char __HeapLimit;
@@ -31,7 +29,6 @@ _sbrk(int incr)
         incr = -incr;
         if (brk - incr < &__HeapBase) {
             prev_brk = (void *)-1;
-            errno = EINVAL;
         } else {
             prev_brk = brk;
             brk -= incr;
@@ -43,7 +40,6 @@ _sbrk(int incr)
             brk += incr;
         } else {
             prev_brk = (void *)-1;
-            errno = ENOMEM;
         }
     }
 
