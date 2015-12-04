fb12884e515dae41c2fd2185b1980da89f4b06ef
Marko Kiiskila <marko@runtime.io>
2015-12-04 13:38:05 -0800

    Remove defines which were under #if 0.

--- libs/nffs/include/nffs/nffs.h
+++ libs/nffs/include/nffs/nffs.h
@@ -20,32 +20,9 @@
 #include <stddef.h>
 #include <inttypes.h>
 
-#if 0
-#define NFFS_ACCESS_READ        0x01
-#define NFFS_ACCESS_WRITE       0x02
-#define NFFS_ACCESS_APPEND      0x04
-#define NFFS_ACCESS_TRUNCATE    0x08
-#endif
-
 #define NFFS_FILENAME_MAX_LEN   256  /* Does not require null terminator. */
-
 #define NFFS_MAX_AREAS          256
-#if 0
-#define NFFS_EOK                0
-#define NFFS_ECORRUPT           1
-#define NFFS_EFLASH_ERROR       2
-#define NFFS_ERANGE             3
-#define NFFS_EINVAL             4
-#define NFFS_ENOMEM             5
-#define NFFS_ENOENT             6
-#define NFFS_EEMPTY             7
-#define NFFS_EFULL              8
-#define NFFS_EUNEXP             9
-#define NFFS_EOS                10
-#define NFFS_EEXIST             11
-#define NFFS_EACCESS            12
-#define NFFS_EUNINIT            13
-#endif
+
 struct nffs_config {
     /** Maximum number of inodes; default=1024. */
     uint32_t nc_num_inodes;
