7117837d1186e0958021b62b389972b34deca4e2
Christopher Collins <ccollins476ad@gmail.com>
2016-02-05 14:40:43 -0800

    bletiny project for small BLE devices.

---
+++ project/bletiny/src/bletiny_priv.h
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2015 Runtime Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef H_BLETINY_PRIV_
+#define H_BLETINY_PRIV_
+
+#include <inttypes.h>
+#include "os/queue.h"
+
+#include "host/ble_gatt.h"
+struct ble_gap_white_entry;
+struct ble_hs_adv_fields;
+struct ble_gap_upd_params;
+struct ble_gap_crt_params;
+struct hci_adv_params;
+struct ble_l2cap_sig_update_req;
+struct ble_l2cap_sig_update_params;
+
+#define BLETINY_MAX_CONNS              8
+
+typedef int cmd_fn(int argc, char **argv);
+struct cmd_entry {
+    char *name;
+    cmd_fn *cb;
+};
+
+struct kv_pair {
+    char *key;
+    int val;
+};
+
+struct bletiny_dsc {
+    SLIST_ENTRY(bletiny_dsc) next;
+    struct ble_gatt_dsc dsc;
+};
+SLIST_HEAD(bletiny_dsc_list, bletiny_dsc);
+
+struct bletiny_chr {
+    SLIST_ENTRY(bletiny_chr) next;
+    struct ble_gatt_chr chr;
+
+    struct bletiny_dsc_list dscs;
+};
+SLIST_HEAD(bletiny_chr_list, bletiny_chr);
+
+struct bletiny_svc {
+    SLIST_ENTRY(bletiny_svc) next;
+    struct ble_gatt_service svc;
+
+    struct bletiny_chr_list chrs;
+};
+
+SLIST_HEAD(bletiny_svc_list, bletiny_svc);
+
+struct bletiny_conn {
+    uint16_t handle;
+    uint8_t addr_type;
+    uint8_t addr[6];
+
+    struct bletiny_svc_list svcs;
+};
+
+extern struct bletiny_conn bletiny_conns[BLETINY_MAX_CONNS];
+extern int bletiny_num_conns;
+
+extern const char *bletiny_device_name;
+extern const uint16_t bletiny_appearance;
+extern const uint8_t bletiny_privacy_flag;
+extern uint8_t bletiny_reconnect_addr[6];
+extern uint8_t bletiny_pref_conn_params[8];
+extern uint8_t bletiny_gatt_service_changed[4];
+
+extern struct nmgr_transport nm_ble_transport;
+extern uint16_t nm_attr_val_handle;
+
+void bletiny_printf(const char *fmt, ...);
+void print_addr(void *addr);
+void print_uuid(void *uuid128);
+const struct cmd_entry *parse_cmd_find(const struct cmd_entry *cmds,
+                                       char *name);
+struct kv_pair *parse_kv_find(struct kv_pair *kvs, char *name);
+char *parse_arg_find(char *key);
+long parse_arg_long_bounds(char *name, long min, long max, int *out_status);
+long parse_arg_long(char *name, int *staus);
+uint16_t parse_arg_uint16(char *name, int *status);
+uint16_t parse_arg_uint16_dflt(char *name, uint16_t dflt, int *out_status);
+uint32_t parse_arg_uint32(char *name, int *out_status);
+int parse_arg_kv(char *name, struct kv_pair *kvs);
+int parse_arg_byte_stream(char *name, int max_len, uint8_t *dst, int *out_len);
+int parse_arg_byte_stream_exact_length(char *name, uint8_t *dst, int len);
+int parse_arg_mac(char *name, uint8_t *dst);
+int parse_arg_uuid(char *name, uint8_t *dst_uuid128);
+int parse_err_too_few_args(char *cmd_name);
+int parse_arg_all(int argc, char **argv);
+int cmd_init(void);
+void periph_init(void);
+int nm_chr_access(uint16_t conn_handle, uint16_t attr_handle,
+                  uint8_t op, union ble_gatt_access_ctxt *ctxt,
+                  void *arg);
+int nm_rx_rsp(uint8_t *attr_val, uint16_t attr_len);
+void nm_init(void);
+void bletiny_lock(void);
+void bletiny_unlock(void);
+int bletiny_exchange_mtu(uint16_t conn_handle);
+int bletiny_disc_svcs(uint16_t conn_handle);
+int bletiny_disc_svc_by_uuid(uint16_t conn_handle, uint8_t *uuid128);
+int bletiny_disc_all_chrs(uint16_t conn_handle, uint16_t start_handle,
+                           uint16_t end_handle);
+int bletiny_disc_chrs_by_uuid(uint16_t conn_handle, uint16_t start_handle,
+                               uint16_t end_handle, uint8_t *uuid128);
+int bletiny_disc_all_dscs(uint16_t conn_handle, uint16_t chr_val_handle,
+                           uint16_t chr_end_handle);
+int bletiny_find_inc_svcs(uint16_t conn_handle, uint16_t start_handle,
+                           uint16_t end_handle);
+int bletiny_read(uint16_t conn_handle, uint16_t attr_handle);
+int bletiny_read_long(uint16_t conn_handle, uint16_t attr_handle);
+int bletiny_read_by_uuid(uint16_t conn_handle, uint16_t start_handle,
+                          uint16_t end_handle, uint8_t *uuid128);
+int bletiny_read_mult(uint16_t conn_handle, uint16_t *attr_handles,
+                       int num_attr_handles);
+int bletiny_write(uint16_t conn_handle, uint16_t attr_handle, void *value,
+                   uint16_t value_len);
+int bletiny_write_no_rsp(uint16_t conn_handle, uint16_t attr_handle,
+                          void *value, uint16_t value_len);
+int bletiny_write_long(uint16_t conn_handle, uint16_t attr_handle,
+                        void *value, uint16_t value_len);
+int bletiny_write_reliable(uint16_t conn_handle, struct ble_gatt_attr *attrs,
+                            int num_attrs);
+int bletiny_adv_start(int disc, int conn, uint8_t *peer_addr, int addr_type,
+                       struct hci_adv_params *params);
+int bletiny_adv_stop(void);
+int bletiny_conn_initiate(int addr_type, uint8_t *peer_addr,
+                           struct ble_gap_crt_params *params);
+int bletiny_conn_cancel(void);
+int bletiny_term_conn(uint16_t conn_handle);
+int bletiny_wl_set(struct ble_gap_white_entry *white_list,
+                    int white_list_count);
+int bletiny_scan(uint32_t dur_ms, uint8_t disc_mode, uint8_t scan_type,
+                  uint8_t filter_policy);
+int bletiny_set_adv_data(struct ble_hs_adv_fields *adv_fields);
+int bletiny_update_conn(uint16_t conn_handle,
+                         struct ble_gap_upd_params *params);
+void bletiny_chrup(uint16_t attr_handle);
+int bletiny_l2cap_update(uint16_t conn_handle,
+                          struct ble_l2cap_sig_update_params *params);
+
+#endif
---
+++ project/bletiny/src/parse.c
@@ -0,0 +1,444 @@
+/**
+ * Copyright (c) 2015 Runtime Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <assert.h>
+#include "console/console.h"
+#include "host/ble_uuid.h"
+#include "bletiny_priv.h"
+
+#define CMD_MAX_ARGS        16
+
+static char *cmd_args[CMD_MAX_ARGS][2];
+static int cmd_num_args;
+
+void
+bletiny_printf(const char *fmt, ...)
+{
+    va_list args;
+
+    if (!console_is_midline) {
+        console_printf("(BLETINY) ");
+    }
+
+    va_start(args, fmt);
+    console_vprintf(fmt, args);
+    va_end(args);
+}
+
+void
+print_addr(void *addr)
+{
+    uint8_t *u8p;
+
+    u8p = addr;
+    bletiny_printf("%02x:%02x:%02x:%02x:%02x:%02x",
+                   u8p[5], u8p[4], u8p[3], u8p[2], u8p[1], u8p[0]);
+}
+
+void
+print_uuid(void *uuid128)
+{
+    uint16_t uuid16;
+    uint8_t *u8p;
+
+    uuid16 = ble_uuid_128_to_16(uuid128);
+    if (uuid16 != 0) {
+        bletiny_printf("0x%04x", uuid16);
+        return;
+    }
+
+    u8p = uuid128;
+
+    /* 00001101-0000-1000-8000-00805f9b34fb */
+    bletiny_printf("%02x%02x%02x%02x-", u8p[15], u8p[14], u8p[13], u8p[12]);
+    bletiny_printf("%02x%02x-%02x%02x-", u8p[11], u8p[10], u8p[9], u8p[8]);
+    bletiny_printf("%02x%02x%02x%02x%02x%02x%02x%02x",
+                   u8p[7], u8p[6], u8p[5], u8p[4],
+                   u8p[3], u8p[2], u8p[1], u8p[0]);
+}
+
+int
+parse_err_too_few_args(char *cmd_name)
+{
+    bletiny_printf("Error: too few arguments for command \"%s\"\n", cmd_name);
+    return -1;
+}
+
+const struct cmd_entry *
+parse_cmd_find(const struct cmd_entry *cmds, char *name)
+{
+    const struct cmd_entry *cmd;
+    int i;
+
+    for (i = 0; cmds[i].name != NULL; i++) {
+        cmd = cmds + i;
+        if (strcmp(name, cmd->name) == 0) {
+            return cmd;
+        }
+    }
+
+    return NULL;
+}
+
+struct kv_pair *
+parse_kv_find(struct kv_pair *kvs, char *name)
+{
+    struct kv_pair *kv;
+    int i;
+
+    for (i = 0; kvs[i].key != NULL; i++) {
+        kv = kvs + i;
+        if (strcmp(name, kv->key) == 0) {
+            return kv;
+        }
+    }
+
+    return NULL;
+}
+
+char *
+parse_arg_find(char *key)
+{
+    int i;
+
+    for (i = 0; i < cmd_num_args; i++) {
+        if (strcmp(cmd_args[i][0], key) == 0) {
+            /* Erase parameter. */
+            cmd_args[i][0][0] = '\0';
+
+            return cmd_args[i][1];
+        }
+    }
+
+    return NULL;
+}
+
+long
+parse_arg_long_bounds(char *name, long min, long max, int *out_status)
+{
+    char *endptr;
+    char *sval;
+    long lval;
+
+    sval = parse_arg_find(name);
+    if (sval == NULL) {
+        *out_status = ENOENT;
+        return 0;
+    }
+
+    lval = strtol(sval, &endptr, 0);
+    if (sval[0] != '\0' && *endptr == '\0' &&
+        lval >= min && lval <= max) {
+
+        *out_status = 0;
+        return lval;
+    }
+
+    *out_status = EINVAL;
+    return 0;
+}
+
+long
+parse_arg_long(char *name, int *out_status)
+{
+    return parse_arg_long_bounds(name, LONG_MIN, LONG_MAX, out_status);
+}
+
+uint16_t
+parse_arg_uint16(char *name, int *out_status)
+{
+    return parse_arg_long_bounds(name, 0, UINT16_MAX, out_status);
+}
+
+uint32_t
+parse_arg_uint32(char *name, int *out_status)
+{
+    return parse_arg_long_bounds(name, 0, UINT32_MAX, out_status);
+}
+
+uint16_t
+parse_arg_uint16_dflt(char *name, uint16_t dflt, int *out_status)
+{
+    uint16_t val;
+    int rc;
+
+    val = parse_arg_uint16(name, &rc);
+    if (rc == ENOENT) {
+        val = dflt;
+        rc = 0;
+    }
+
+    *out_status = rc;
+    return val;
+}
+
+int
+parse_arg_kv(char *name, struct kv_pair *kvs)
+{
+    struct kv_pair *kv;
+    char *sval;
+
+    sval = parse_arg_find(name);
+    if (sval == NULL) {
+        return ENOENT;
+    }
+
+    kv = parse_kv_find(kvs, sval);
+    if (kv == NULL) {
+        return EINVAL;
+    }
+
+    return kv->val;
+}
+
+static int
+parse_arg_byte_stream_no_delim(char *sval, int max_len, uint8_t *dst,
+                               int *out_len)
+{
+    unsigned long ul;
+    char *endptr;
+    char buf[3];
+    int i;
+
+    buf[2] = '\0';
+    i = 0;
+    while (1) {
+        if (sval[i * 2] == '\0') {
+            *out_len = i;
+            return 0;
+        }
+
+        if (i >= max_len) {
+            return EINVAL;
+        }
+
+        buf[0] = sval[i * 2 + 0];
+        buf[1] = sval[i * 2 + 1];
+
+        ul = strtoul(buf, &endptr, 16);
+        if (*sval == '\0' || *endptr != '\0') {
+            return EINVAL;
+        }
+
+        assert(ul <= UINT8_MAX);
+        dst[i] = ul;
+
+        i++;
+    }
+}
+
+static int
+parse_arg_byte_stream_delim(char *sval, char *delims, int max_len,
+                            uint8_t *dst, int *out_len)
+{
+    unsigned long ul;
+    char *endptr;
+    char *token;
+    int i;
+
+    i = 0;
+    for (token = strtok(sval, delims);
+         token != NULL;
+         token = strtok(NULL, delims)) {
+
+        if (i >= max_len) {
+            return EINVAL;
+        }
+
+        ul = strtoul(token, &endptr, 16);
+        if (sval[0] == '\0' || *endptr != '\0' || ul > UINT8_MAX) {
+            return -1;
+        }
+
+        dst[i] = ul;
+        i++;
+    }
+
+    *out_len = i;
+
+    return 0;
+}
+
+int
+parse_arg_byte_stream(char *name, int max_len, uint8_t *dst, int *out_len)
+{
+    int total_len;
+    char *sval;
+
+    sval = parse_arg_find(name);
+    if (sval == NULL) {
+        return ENOENT;
+    }
+
+    total_len = strlen(sval);
+    if (strcspn(sval, ":-") == total_len) {
+        return parse_arg_byte_stream_no_delim(sval, max_len, dst, out_len);
+    } else {
+        return parse_arg_byte_stream_delim(sval, ":-", max_len, dst, out_len);
+    }
+}
+
+int
+parse_arg_byte_stream_exact_length(char *name, uint8_t *dst, int len)
+{
+    int actual_len;
+    int rc;
+
+    rc = parse_arg_byte_stream(name, 6, dst, &actual_len);
+    if (rc != 0) {
+        return rc;
+    }
+
+    if (actual_len != len) {
+        return EINVAL;
+    }
+
+    return 0;
+}
+
+static void
+parse_reverse_bytes(uint8_t *bytes, int len)
+{
+    uint8_t tmp;
+    int i;
+
+    for (i = 0; i < len / 2; i++) {
+        tmp = bytes[i];
+        bytes[i] = bytes[len - i - 1];
+        bytes[len - i - 1] = tmp;
+    }
+}
+
+int
+parse_arg_mac(char *name, uint8_t *dst)
+{
+    int rc;
+
+    rc = parse_arg_byte_stream_exact_length(name, dst, 6);
+    if (rc != 0) {
+        return rc;
+    }
+
+    parse_reverse_bytes(dst, 6);
+
+    return 0;
+}
+
+int
+parse_arg_uuid(char *str, uint8_t *dst_uuid128)
+{
+    uint16_t uuid16;
+    char *tok;
+    int rc;
+
+    uuid16 = parse_arg_uint16(str, &rc);
+    switch (rc) {
+    case ENOENT:
+        return ENOENT;
+
+    case 0:
+        rc = ble_uuid_16_to_128(uuid16, dst_uuid128);
+        if (rc != 0) {
+            return EINVAL;
+        } else {
+            return 0;
+        }
+
+    default:
+        /* e7add801-b042-4876-aae1112855353cc1 */
+        if (strlen(str) == 35) {
+            tok = strtok(str, "-");
+            if (tok == NULL) {
+                return EINVAL;
+            }
+            rc = parse_arg_byte_stream_exact_length(tok, dst_uuid128 + 0, 4);
+            if (rc != 0) {
+                return rc;
+            }
+
+            tok = strtok(NULL, "-");
+            if (tok == NULL) {
+                return EINVAL;
+            }
+            rc = parse_arg_byte_stream_exact_length(tok, dst_uuid128 + 4, 2);
+            if (rc != 0) {
+                return rc;
+            }
+
+            tok = strtok(NULL, "-");
+            if (tok == NULL) {
+                return EINVAL;
+            }
+            rc = parse_arg_byte_stream_exact_length(tok, dst_uuid128 + 6, 2);
+            if (rc != 0) {
+                return rc;
+            }
+
+            tok = strtok(NULL, "-");
+            if (tok == NULL) {
+                return EINVAL;
+            }
+            rc = parse_arg_byte_stream_exact_length(tok, dst_uuid128 + 8, 8);
+            if (rc != 0) {
+                return rc;
+            }
+
+            return 0;
+        }
+
+        rc = parse_arg_byte_stream_exact_length(str, dst_uuid128, 16);
+        return rc;
+    }
+}
+
+int
+parse_arg_all(int argc, char **argv)
+{
+    char *key;
+    char *val;
+    int i;
+
+    cmd_num_args = 0;
+
+    for (i = 0; i < argc; i++) {
+        key = strtok(argv[i], "=");
+        val = strtok(NULL, "=");
+
+        if (key != NULL && val != NULL) {
+            if (strlen(key) == 0) {
+                bletiny_printf("Error: invalid argument: %s\n", argv[i]);
+                return -1;
+            }
+
+            if (cmd_num_args >= CMD_MAX_ARGS) {
+                bletiny_printf("Error: too many arguments");
+                return -1;
+            }
+
+            cmd_args[cmd_num_args][0] = key;
+            cmd_args[cmd_num_args][1] = val;
+            cmd_num_args++;
+        }
+    }
+
+    return 0;
+}
+
