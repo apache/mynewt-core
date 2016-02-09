6153883a100b5149c90514237ada23d56cfd88de
Paul Dietrich <paulfdietrich@yahoo.com>
2016-02-09 09:36:55 -0800

    change this so it does not stop openocd with ctrl-c

--- hw/bsp/arduino_zero/arduino_zero_debug.sh
+++ hw/bsp/arduino_zero/arduino_zero_debug.sh
@@ -16,11 +16,16 @@ GDB_CMD_FILE=.gdb_cmds
 
 echo "Debugging" $FILE_NAME
 
-openocd -f arduino_zero.cfg -c "gdb_port 3333;init;reset halt" &
+#
+# Block Ctrl-C from getting passed to openocd.
+# Exit openocd when gdb detaches.
+#
+set -m
+openocd -f arduino_zero.cfg -c "gdb_port 3333;telnet_port 4444; init;reset halt" &
+set +m
 echo "target remote localhost:3333 " > $GDB_CMD_FILE
 arm-none-eabi-gdb -x $GDB_CMD_FILE $FILE_NAME
 rm $GDB_CMD_FILE
-
 
  
 
