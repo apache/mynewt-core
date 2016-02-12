fa169d3d2e1354eb527c04f83ff37e9795cb7185
Christopher Collins <ccollins476ad@gmail.com>
2016-02-12 14:09:24 -0800

    BLE host - only check mutex state if OS started.

--- net/nimble/host/src/ble_hs_misc.c
+++ net/nimble/host/src/ble_hs_misc.c
@@ -74,10 +74,12 @@ ble_hs_misc_log_flat_buf(void *data, int len)
 void
 ble_hs_misc_assert_no_locks(void)
 {
-    assert(!ble_hs_conn_locked_by_cur_task());
-    assert(!ble_gattc_locked_by_cur_task());
-    assert(!ble_gap_locked_by_cur_task());
-    assert(!ble_hci_sched_locked_by_cur_task());
+    if (os_started()) {
+        assert(!ble_hs_conn_locked_by_cur_task());
+        assert(!ble_gattc_locked_by_cur_task());
+        assert(!ble_gap_locked_by_cur_task());
+        assert(!ble_hci_sched_locked_by_cur_task());
+    }
 }
 
 /**
