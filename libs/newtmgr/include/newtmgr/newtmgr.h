16dce45daa1dca2b0c354d0078ef579905997385
Sterling Hughes <sterling@apache.org>
2016-02-23 15:31:42 -0800

    This closes #6
    Squashed commit of the following:
    
    commit a91d0a934b1f2e70ff13f6f46a578d757558ddd3
    Author: Gordon Chaffee <gordon@runtime.io>
    Date:   Mon Feb 22 12:39:22 2016 -0800
    
        Add definition of LOGS_NMGR_OP_CLEAR
    
    commit bdfac9a0ed4d0fc4d39d43e9b763432eb8393641
    Author: Gordon Chaffee <gordon@runtime.io>
    Date:   Mon Feb 22 03:32:54 2016 -0800
    
        Add stub for newtmgr 'logs clear' command
    
    commit e5f11daedad980570f6267c3d7f4e03af01aa985
    Author: Gordon Chaffee <gordon@runtime.io>
    Date:   Mon Feb 22 03:26:29 2016 -0800
    
        Add support for newtmgr 'logs show' command
    
    commit e1a67326d5d8ecc6fb95dfe4576fe1c90ffd39d2
    Author: Gordon Chaffee <gordon@runtime.io>
    Date:   Mon Feb 22 03:21:54 2016 -0800
    
        Add encoding functions for json arrays of unknown length

--- libs/newtmgr/include/newtmgr/newtmgr.h
+++ libs/newtmgr/include/newtmgr/newtmgr.h
@@ -29,8 +29,9 @@
  */
 #define NMGR_GROUP_ID_DEFAULT   (0)
 #define NMGR_GROUP_ID_IMAGE     (1)
-#define NMGR_GROUP_ID_STATS     (2) 
+#define NMGR_GROUP_ID_STATS     (2)
 #define NMGR_GROUP_ID_CONFIG    (3)
+#define NMGR_GROUP_ID_LOGS      (4)
 #define NMGR_GROUP_ID_PERUSER   (64)
 
 #define NMGR_OP_READ            (0)
