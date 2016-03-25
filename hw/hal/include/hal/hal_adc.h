23ba5b4c656018010be3ca080b3f2da07cd5de53
Paul Dietrich <paulfdietrich@yahoo.com>
2016-03-25 13:51:42 -0700

    third approach to mynewt HAL.
    forgot two files
    
    added an implementation for sim
    
    fix output of file adc

---
+++ hw/hal/include/hal/hal_adc.h
@@ -0,0 +1,65 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef HAL_ADC_H
+#define HAL_ADC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <bsp/bsp.h>    
+struct hal_adc_device_s; 
+
+/* initialize the ADC corresponding to adc device padc.
+ * Returns 0 on success.  the ADC must be initialized
+ * before calling any other functions in this header file .
+ * If other methods are called with a sysid before init is 
+ * called, the function will return error */
+int hal_adc_init(struct hal_adc_device_s *padc, enum BspPinDescriptor pin);
+
+/*
+ * read the ADC corresponding to sysid in your system.  Returns
+ * the  adc value read or negative on error, See 
+ * hal_adc_get_resolution to check the range of the return value */
+int hal_adc_read(struct hal_adc_device_s *padc);
+
+/* returns the number of bit of resolution in this ADC.  
+ * For example if the system has an 8-bit ADC reporting 
+ * values from 0= to 255 (2^8-1), this function would return
+ * the value 8. returns negative or zero on error */
+int hal_adc_get_resolution(struct hal_adc_device_s *padc);
+
+/* Returns the positive reference voltage for a maximum ADC reading.
+ * This API assumes the negative reference voltage is zero volt.
+ * Returns negative or zero on error.  
+ */
+int hal_adc_get_reference_voltage_mvolts(struct hal_adc_device_s *padc);
+
+/* Converts and ADC value to millivolts. This is a helper function
+ * but does call the ADC to get the reference voltage and 
+ * resolution */
+int hal_adc_val_convert_to_mvolts(struct hal_adc_device_s *padc, int val);
+ 
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAL_ADC_H */
+
--- hw/mcu/native/include/mcu/native_bsp.h
+++ hw/mcu/native/include/mcu/native_bsp.h
@@ -19,6 +19,33 @@
 #ifndef H_NATIVE_BSP_
 #define H_NATIVE_BSP_
 
+#include <stdio.h>
+
 extern const struct hal_flash native_flash_dev;
+
+/* this is a native build and these pins are not real.  They are used only
+ * for simulated HAL devices */
+enum McuPinDescriptor {
+    MCU_PIN_0   = 0,
+    MCU_PIN_1   = 1,
+    MCU_PIN_2   = 2,
+    MCU_PIN_3   = 3,
+    MCU_PIN_4   = 4,
+    MCU_PIN_5   = 5,
+};
+
+
+enum adc_channel_type {
+    ADC_RANDOM,
+    ADC_MIN,
+    ADC_MID,
+    ADC_MAX,
+    ADC_FILE
+};
+
+struct hal_adc_device_s {
+    enum adc_channel_type type;
+    FILE *native_fs;
+};
 
 #endif /* H_NATIVE_BSP_ */
