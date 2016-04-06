8fc5098e8812da06e3e9b89eb75848ea8cb79cd5
Christopher Collins <ccollins@apache.org>
2016-04-06 16:49:17 -0700

    MYNEWT-265 BLE - advertise iBeacon
    Blocking HCI transmits.

--- net/nimble/host/include/host/host_hci.h
+++ net/nimble/host/include/host/host_hci.h
@@ -26,6 +26,7 @@ struct os_mbuf;
 
 int host_hci_os_event_proc(struct os_event *ev);
 int host_hci_event_rx(uint8_t *data);
+uint16_t host_hci_opcode_join(uint8_t ogf, uint16_t ocf);
 int host_hci_cmd_send(uint8_t ogf, uint8_t ocf, uint8_t len, void *cmddata);
 int host_hci_cmd_set_event_mask(uint64_t event_mask);
 int host_hci_cmd_disconnect(uint16_t handle, uint8_t reason);
