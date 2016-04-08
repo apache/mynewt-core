46849c552bb1b2687c3c47f8efa0c39af23c3ed9
Marko Kiiskila <marko@runtime.io>
2016-04-08 10:36:26 -0700

    config; add CLI command to dump current running config.

--- sys/config/src/config_cli.c
+++ sys/config/src/config_cli.c
@@ -35,6 +35,24 @@ static struct shell_cmd shell_conf_cmd = {
     .sc_cmd_func = shell_conf_command
 };
 
+static void
+conf_dump_one(struct conf_handler *ch, char *name, char *val)
+{
+    console_printf("%s/%s = %s\n", ch->ch_name, name, val ? val : "<del>");
+}
+
+static void
+conf_dump(void)
+{
+    struct conf_handler *ch;
+
+    SLIST_FOREACH(ch, &conf_handlers, ch_list) {
+        if (ch->ch_export) {
+            ch->ch_export(conf_dump_one);
+        }
+    }
+}
+
 static int
 shell_conf_command(int argc, char **argv)
 {
@@ -63,6 +81,12 @@ shell_conf_command(int argc, char **argv)
             val = "Done\n";
         }
         console_printf("%s", val);
+        return 0;
+    }
+    if (!strcmp(name, "dump")) {
+        if (!val || !strcmp(val, "running")) {
+            conf_dump();
+        }
         return 0;
     }
     if (!val) {
