c56aca4ac6f78f626792e1fc6d9f01e6ebc81d38
Marko Kiiskila <marko@runtime.io>
2016-04-13 10:54:24 -0700

    fs; add 'cat' CLI command.

--- fs/fs/src/fs_cli.c
+++ fs/fs/src/fs_cli.c
@@ -31,6 +31,7 @@ static int fs_ls_cmd(int argc, char **argv);
 static int fs_rm_cmd(int argc, char **argv);
 static int fs_mkdir_cmd(int argc, char **argv);
 static int fs_mv_cmd(int argc, char **argv);
+static int fs_cat_cmd(int argc, char **argv);
 
 static struct shell_cmd fs_ls_struct = {
     .sc_cmd = "ls",
@@ -47,6 +48,10 @@ static struct shell_cmd fs_mkdir_struct = {
 static struct shell_cmd fs_mv_struct = {
     .sc_cmd = "mv",
     .sc_cmd_func = fs_mv_cmd
+};
+static struct shell_cmd fs_cat_struct = {
+    .sc_cmd = "cat",
+    .sc_cmd_func = fs_cat_cmd
 };
 
 static void
@@ -180,6 +185,39 @@ out:
     return 0;
 }
 
+static int
+fs_cat_cmd(int argc, char **argv)
+{
+    int rc;
+    struct fs_file *file;
+    char buf[32];
+    uint32_t len;
+
+    if (argc != 2) {
+        console_printf("cat <filename>\n");
+        return -1;
+    }
+
+    rc = fs_open(argv[1], FS_ACCESS_READ, &file);
+    if (rc != FS_EOK) {
+        console_printf("Error opening %s - %d\n", argv[1], rc);
+        return -1;
+    }
+
+    do {
+        rc = fs_read(file, sizeof(buf), buf, &len);
+        if (rc != FS_EOK) {
+            console_printf("\nError reading %s - %d\n", argv[1], rc);
+            break;
+        }
+        console_write(buf, len);
+    } while (len > 0);
+
+    fs_close(file);
+
+    return 0;
+}
+
 void
 fs_cli_init(void)
 {
@@ -187,5 +225,6 @@ fs_cli_init(void)
     shell_cmd_register(&fs_rm_struct);
     shell_cmd_register(&fs_mkdir_struct);
     shell_cmd_register(&fs_mv_struct);
+    shell_cmd_register(&fs_cat_struct);
 }
 #endif /* SHELL_PRESENT */
