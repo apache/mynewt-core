09e50c6a08883bdc284fca54e13995b9a4b1a799
Christopher Collins <ccollins@apache.org>
2016-04-14 16:37:26 -0700

    Fix for 64-bit types passed to *printf.
    The va_arg macro from newlib seems to have issues with 64-bit values; it
    was reading from the wrong memory location.  The temporary fix is to
    pull the 64-bit number off the stack as a pair of 32-bit integers.
    
    We need to look into why this is necessary.  It may just be an incorrect
    setting specified when the compiler / newlib was built.

--- libs/baselibc/src/tinyprintf.c
+++ libs/baselibc/src/tinyprintf.c
@@ -209,11 +209,15 @@ intarg(int lng, int sign, va_list *va)
 
     case 2:
     default:
-        if (sign) {
-            val = va_arg(*va, long long);
-        } else {
-            val = va_arg(*va, unsigned long long);
-        }
+        /* Pull the 64-bit number off the stack as a pair of 32-bit integers.
+         * The va_arg macro was reading from the wrong location when a 64-bit
+         * type was specified.
+         */
+        /* XXX: Look into this; may just be an incorrect setting when the
+         * compiler / newlib was built.
+         */
+        val = va_arg(*va, unsigned long);
+        val |= (unsigned long long)(va_arg(*va, unsigned long)) << 32;
         break;
     }
 
@@ -226,7 +230,7 @@ size_t tfp_format(FILE *putp, const char *fmt, va_list va)
     struct param p;
     char bf[23];
     char ch;
-    char lng = 0;
+    char lng;
 
     p.bf = bf;
 
@@ -239,6 +243,7 @@ size_t tfp_format(FILE *putp, const char *fmt, va_list va)
             p.alt = 0;
             p.width = 0;
             p.sign = 0;
+            lng = 0;
 
             /* Flags */
             while ((ch = *(fmt++))) {
