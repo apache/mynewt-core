88356a0df1811e5905588116f6c7cab62764e6ad
Marko Kiiskila <marko@runtime.io>
2016-04-21 12:58:16 -0700

    console; instead of poking libs/os var g_os_started, use the provided API call.

--- libs/console/full/src/cons_tty.c
+++ libs/console/full/src/cons_tty.c
@@ -102,7 +102,7 @@ console_queue_char(char ch)
         /* TX needs to drain */
         hal_uart_start_tx(CONSOLE_UART);
         OS_EXIT_CRITICAL(sr);
-	if (g_os_started) {
+	if (os_started()) {
             os_time_delay(1);
 	}
         OS_ENTER_CRITICAL(sr);
