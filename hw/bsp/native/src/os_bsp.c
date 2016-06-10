d92b6bb91d1efca598057f6448881cbd3496c892
Vipul Rahane <vipul@runtime.io>
2016-06-10 12:20:00 -0700

    Fixing flash native map: Unit tests detected this

--- hw/bsp/native/src/os_bsp.c
+++ hw/bsp/native/src/os_bsp.c
@@ -23,14 +23,9 @@ static struct flash_area bsp_flash_areas[] = {
     [FLASH_AREA_BOOTLOADER] = {
         .fa_flash_id = 0,       /* internal flash */
         .fa_off = 0x00000000,   /* beginning */
-        .fa_size = (32 * 1024)
-    },
-    [FLASH_AREA_REBOOT_LOG] = {
-        .fa_flash_id = 0,
-        .fa_off = 0x00010000,
         .fa_size = (16 * 1024)
     },
-    /* 1 * 16K and 1*64K sectors here */
+    /* 2 * 16K and 1*64K sectors here */
     [FLASH_AREA_IMAGE_0] = {
         .fa_flash_id = 0,
         .fa_off = 0x00020000,
@@ -50,7 +45,12 @@ static struct flash_area bsp_flash_areas[] = {
         .fa_flash_id = 0,
         .fa_off = 0x00008000,
         .fa_size = (32 * 1024)
-    }
+    },
+    [FLASH_AREA_REBOOT_LOG] = {
+        .fa_flash_id = 0,
+        .fa_off = 0x00004000,
+        .fa_size = (16 * 1024)
+    },
 };
 
 /*
