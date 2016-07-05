faed993b3d9295fd402408eebcbb3f0258f7cf67
Christopher Collins <ccollins@apache.org>
2016-07-05 12:18:03 -0700

    BLE Host - Remove obsolete error codes.

--- net/nimble/host/include/host/ble_hs.h
+++ net/nimble/host/include/host/ble_hs.h
@@ -47,18 +47,16 @@ struct os_event;
 #define BLE_HS_EAPP                 9
 #define BLE_HS_EBADDATA             10
 #define BLE_HS_EOS                  11
-#define BLE_HS_ECONGESTED           12
-#define BLE_HS_ECONTROLLER          13
-#define BLE_HS_ETIMEOUT             14
-#define BLE_HS_EDONE                15
-#define BLE_HS_EBUSY                16
-#define BLE_HS_EREJECT              17
-#define BLE_HS_EUNKNOWN             18
-#define BLE_HS_EROLE                19
-#define BLE_HS_ETIMEOUT_HCI         20
-#define BLE_HS_ENOMEM_HCI           21
-#define BLE_HS_ENOMEM_EVT           22
-#define BLE_HS_ENOADDR              23
+#define BLE_HS_ECONTROLLER          12
+#define BLE_HS_ETIMEOUT             13
+#define BLE_HS_EDONE                14
+#define BLE_HS_EBUSY                15
+#define BLE_HS_EREJECT              16
+#define BLE_HS_EUNKNOWN             17
+#define BLE_HS_EROLE                18
+#define BLE_HS_ETIMEOUT_HCI         19
+#define BLE_HS_ENOMEM_EVT           20
+#define BLE_HS_ENOADDR              21
 
 #define BLE_HS_ERR_ATT_BASE         0x100   /* 256 */
 #define BLE_HS_ATT_ERR(x)           ((x) ? BLE_HS_ERR_ATT_BASE + (x) : 0)
