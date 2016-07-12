f27fdb23d5628a7f5ea8beb5d0293b1679f67aaf
Christopher Collins <ccollins@apache.org>
2016-07-11 20:22:53 -0700

    os - Move callout list init from data to text
    This ensures the callout list gets initialized every time the OS is
    initialized.  There were issues with stale callouts when unit tests
    restarted the OS.

--- libs/os/src/os_priv.h
+++ libs/os/src/os_priv.h
@@ -21,10 +21,12 @@
 #define H_OS_PRIV_
 
 TAILQ_HEAD(os_task_list, os_task);
+TAILQ_HEAD(os_callout_list, os_callout);
 
 extern struct os_task_list g_os_run_list;
 extern struct os_task_list g_os_sleep_list;
 extern struct os_task_list g_os_task_list;
 extern struct os_task *g_current_task;
+extern struct os_callout_list g_callout_list;
 
 #endif
