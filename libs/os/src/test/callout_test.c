3359c33c0c7ad49f0c8c4d1d3b4254c86cafee26
NgesBrian <nges.brian@gmail.com>
2016-07-24 11:19:49 +0100

    adding the test for os_callout

--- libs/os/src/test/callout_test.c
+++ libs/os/src/test/callout_test.c
@@ -29,40 +29,61 @@
 struct os_task callout_task_struct_send;
 os_stack_t callout_task_stack_send[CALLOUT_STACK_SIZE];
 
-/* Initializing the callout 
-struct os_event callout_event;
-struct os_eventq callout_eventq;
-struct os_eventq callout_eventq_delivered;
-struct os_callout struct_callout;
-*/
+#define RECEIVE_CALLOUT_TASK_PRIO        (2)
+struct os_task callout_task_struct_receive;
+os_stack_t callout_task_stack_receive[CALLOUT_STACK_SIZE];
 
 /* Delearing variables for callout_func */
-struct os_callout_func callout_func;
-struct os_eventq callout_eventq;
+struct os_callout_func callout_func_test;
+struct os_eventq *callout_eventq;
 void native_cputimer_cb(void *arg);
+
+/* The event to be sent*/
+struct os_eventq callout_evq;
+struct os_event callout_ev;
 
 /* This is a callout task to send data */
 void
 callout_task_send()
 {
-    int i;
-    /*
-    struct_callout.c_ev = callout_event;
-    struct_callout.c_evq = &callout_eventq;
-    struct_callout.c_ticks = OS_TICKS_PER_SEC / 20;
-    */
-    /* Initialize the callout
-    os_callout_init(&struct_callout, &callout_eventq_delivered, NULL);
-    */
+   int i;
+    /* assigning the *callout_eventq */
+    callout_eventq = &callout_evq;
 
     /* Initialize the callout function */
-    os_callout_func_init(&callout_func, &callout_eventq,
+    os_callout_func_init(&callout_func_test, callout_eventq,
         native_cputimer_cb, NULL);
-    i = os_callout_queued((struct os_callout )callout_func);
+
+    /* should say whether os is armed or not */
+    i = os_callout_queued(&callout_func_test.cf_c);
+    TEST_ASSERT(i == 0);
+    
+    /* Arm the OS */
+    os_callout_reset(&callout_func_test.cf_c, OS_TICKS_PER_SEC/ 50);
+    
+    /* Send the callout */ 
+    os_time_delay(OS_TICKS_PER_SEC / 50);
     TEST_ASSERT(i == 0);
 }
 
-TEST_CASE(callout_test){
+void
+callout_task_receive(void *arg)
+{
+    struct os_event *event;
+    os_time_delay(OS_TICKS_PER_SEC / 20);
+    /* Recieving using the os_eventq_poll*/
+        event = os_eventq_poll(&callout_func_test.cf_c.c_evq, 1, OS_WAIT_FOREVER);
+        TEST_ASSERT(event->ev_type ==  OS_EVENT_T_TIMER);
+        TEST_ASSERT(event->ev_arg == NULL);
+
+
+    /* Finishes the test when OS has been started */
+    os_test_restart();
+
+}
+
+TEST_CASE(callout_test)
+{
 
     /* Initializing the OS */
     os_init();
@@ -72,11 +93,19 @@ TEST_CASE(callout_test){
         callout_task_send, NULL, SEND_CALLOUT_TASK_PRIO, OS_WAIT_FOREVER,
         callout_task_stack_send, CALLOUT_STACK_SIZE);
 
+    /* Initialize the sending task */
+    os_task_init(&callout_task_struct_receive, "callout_task_receive",
+        callout_task_receive, NULL, RECEIVE_CALLOUT_TASK_PRIO, OS_WAIT_FOREVER,
+        callout_task_stack_receive, CALLOUT_STACK_SIZE);
+
+    os_eventq_init(callout_eventq);
+    
     /* Does not return until OS_restart is called */
     os_start();
 
 }
 
-TEST_SUITE(os_callout_test_suite){   
+TEST_SUITE(os_callout_test_suite)
+{   
     callout_test();
 }
