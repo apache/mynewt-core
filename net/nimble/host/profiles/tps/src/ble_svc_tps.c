b4ae19f09222d57f5d446d0b163d79ace8725f11
Brian Giori <briangiori@gmail.com>
2016-07-28 09:37:02 -0700

    Tx Power Service implementation

--- net/nimble/host/profiles/tps/src/ble_svc_tps.c
+++ net/nimble/host/profiles/tps/src/ble_svc_tps.c
@@ -59,6 +59,7 @@ ble_svc_tps_access(uint16_t conn_handle, uint16_t attr_handle,
                    struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
     assert(ctxt->chr == &ble_svc_tps_defs[0].characteristics[0]);
+    int rc;
     switch(ctxt->op) {
     case BLE_GATT_ACCESS_OP_READ_CHR:
         ble_hci_util_read_adv_tx_pwr(&ble_svc_tps_tx_power_level);
@@ -87,7 +88,7 @@ ble_svc_tps_init(struct ble_hs_cfg *cfg)
         return rc;
     }
 
-    rc = ble_gatts_add_svcs(ble_svc_lls_defs);
+    rc = ble_gatts_add_svcs(ble_svc_tps_defs);
     if (rc != 0) {
         return rc;
     }
