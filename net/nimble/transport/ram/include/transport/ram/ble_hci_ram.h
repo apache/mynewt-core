a7f8b7ac6dec6551c55fb4237b086373b15fa67b
Christopher Collins <ccollins@apache.org>
2016-08-04 16:31:05 -0700

    NimBLE - high / low prio event buffers.
    Events have one of two priorities:
    o Low-priority   (BLE_HCI_TRANS_BUF_EVT_LO)
    o High-priority  (BLE_HCI_TRANS_BUF_EVT_HI)
    
    Low-priority event buffers are only used for advertising reports.  If
    there are no free low-priority event buffers, then an incoming
    advertising report will get dropped.
    
    High-priority event buffers are for everything except advertising
    reports.  If there are no free high-priority event buffers, a request to
    allocate one will try to allocate a low-priority buffer instead.
    
    If you want all events to be given equal treatment, then you should
    allocate low-priority events only.
    
    Event priorities solve the problem of critical events getting dropped
    due to a flood of advertising reports.  This solution is likely
    temporary: when HCI flow control is added, event priorities may become
    obsolete.
    
    Not all transports distinguish between low and high priority events.  If
    the transport does not have separate settings for low and high buffer
    counts, then it treats all events with equal priority.

--- net/nimble/transport/ram/include/transport/ram/ble_hci_ram.h
+++ net/nimble/transport/ram/include/transport/ram/ble_hci_ram.h
@@ -4,8 +4,23 @@
 #include "nimble/ble_hci_trans.h"
 
 struct ble_hci_ram_cfg {
-    uint16_t num_evt_bufs;
+    /** Number of high-priority event buffers. */
+    uint16_t num_evt_hi_bufs;
+
+    /** Number of low-priority event buffers. */
+    uint16_t num_evt_lo_bufs;
+
+    /** Size of each event buffer, in bytes. */
     uint16_t evt_buf_sz;
+
+    /* Note: For information about high-priority vs. low-priority event
+     * buffers, see net/nimble/include/nimble/ble_hci_trans.h.
+     */
+
+    /* Note: host-to-controller command buffers are not configurable.  The RAM
+     * transport only allows one outstanding command, so it uses a single
+     * statically-allocated buffer.
+     */
 };
 
 extern const struct ble_hci_ram_cfg ble_hci_ram_cfg_dflt;
