711b31dd3eee0da8952a44a929304d7791af2c35
Fabio Utzig <utzig@utzig.org>
2016-08-28 19:23:32 -0300

    Fix identation

--- libs/console/full/src/cons_tty.c
+++ libs/console/full/src/cons_tty.c
@@ -117,9 +117,9 @@ console_queue_char(char ch)
         /* TX needs to drain */
         uart_start_tx(ct->ct_dev);
         OS_EXIT_CRITICAL(sr);
-    if (os_started()) {
+        if (os_started()) {
             os_time_delay(1);
-    }
+        }
         OS_ENTER_CRITICAL(sr);
     }
     console_add_char(&ct->ct_tx, ch);
