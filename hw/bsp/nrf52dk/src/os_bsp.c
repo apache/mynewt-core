158051c199371a80efc0a4fabfb28d2b6af4f9b6
Christopher Collins <ccollins@apache.org>
2016-09-30 13:32:25 -0700

    nrf52dk - Enable reboot log.

--- hw/bsp/nrf52dk/src/os_bsp.c
+++ hw/bsp/nrf52dk/src/os_bsp.c
@@ -52,7 +52,7 @@ static struct flash_area bsp_flash_areas[] = {
     [FLASH_AREA_BOOTLOADER] = {
         .fa_flash_id = 0,       /* internal flash */
         .fa_off = 0x00000000,   /* beginning */
-        .fa_size = (32 * 1024)
+        .fa_size = (16 * 1024)
     },
     /* 2*16K and 1*64K sectors here */
     [FLASH_AREA_IMAGE_0] = {
@@ -74,6 +74,11 @@ static struct flash_area bsp_flash_areas[] = {
         .fa_flash_id = 0,
         .fa_off = 0x0007d000,
         .fa_size = (12 * 1024)
+    },
+    [FLASH_AREA_REBOOT_LOG] = {
+        .fa_flash_id = 0,
+        .fa_off = 0x00004000,
+        .fa_size = (16 * 1024)
     }
 };
 
