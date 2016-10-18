4cd92663888e0cfd1d1801f06a49d48ea501ec98
William San Filippo <wills@runtime.io>
2016-10-17 21:06:14 -0700

    MYNEWT-437: Add watchdog for stm32f407
    This change adds the hal for the stm32f407. Since the maximum
    independent watchdog timeout is 32.768 seconds, I decided to
    commit a different sanity and watchdog interval for the OS.
    I changed them to 15 seconds and 30 seconds, respectively.

--- hw/mcu/stm/stm32f4xx/src/hal_watchdog.c
+++ hw/mcu/stm/stm32f4xx/src/hal_watchdog.c
@@ -18,20 +18,43 @@
  */
 
 #include "hal/hal_watchdog.h"
+#include "stm32f407xx.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_iwdg.h"
+
+IWDG_HandleTypeDef g_wdt_cfg;
 
 int
 hal_watchdog_init(uint32_t expire_msecs)
 {
-    return (0);
+    uint32_t reload;
+
+    /* Max prescaler is 256 */
+    reload = 32768 / 256;
+    reload = (reload * expire_msecs) / 1000;
+
+    /* Check to make sure we are not trying a reload value that is too large */
+    if (reload > IWDG_RLR_RL) {
+        return -1;
+    }
+
+    g_wdt_cfg.Instance = IWDG;
+    g_wdt_cfg.Init.Prescaler = IWDG_PRESCALER_256;
+    g_wdt_cfg.Init.Reload = reload;
+
+    return 0;
 }
 
 void
 hal_watchdog_enable(void)
 {
+    __HAL_DBGMCU_FREEZE_IWDG();
+    HAL_IWDG_Init(&g_wdt_cfg);
 }
 
 void
 hal_watchdog_tickle(void)
 {
+    HAL_IWDG_Refresh(&g_wdt_cfg);
 }
 
