39a06ff5895ba42a981dcb6a24b03e3b11336d8f
Marko Kiiskila <marko@runtime.io>
2016-10-27 20:44:06 -0700

    oic; make resource allocations configurable with syscfg.

--- net/oic/src/port/mynewt/config.h
+++ net/oic/src/port/mynewt/config.h
@@ -7,10 +7,10 @@ extern "C" {
 #endif
 
 /* Time resolution */
-#include <stdint.h>
+#include <inttypes.h>
+#include <syscfg/syscfg.h>
 #include <os/os.h>
 #include <log/log.h>
-#include <syscfg/syscfg.h>
 
 /* rather than change all their source files, just translate the mynewt
  * package defines into their defines here */
@@ -43,26 +43,26 @@ typedef os_time_t oc_clock_time_t;
 
 /* Server-side parameters */
 /* Maximum number of server resources */
-#define MAX_APP_RESOURCES (8)
+#define MAX_APP_RESOURCES MYNEWT_VAL(OC_APP_RESOURCES)
 
 /* Common paramters */
 /* Maximum number of concurrent requests */
-#define MAX_NUM_CONCURRENT_REQUESTS (2)
+#define MAX_NUM_CONCURRENT_REQUESTS MYNEWT_VAL(OC_CONCURRENT_REQUESTS)
 
 /* Estimated number of nodes in payload tree structure */
-#define EST_NUM_REP_OBJECTS (100)
+#define EST_NUM_REP_OBJECTS MYNEWT_VAL(OC_NUM_REP_OBJECTS)
 
 /* Maximum size of request/response PDUs */
-#define MAX_PAYLOAD_SIZE (1120)
+#define MAX_PAYLOAD_SIZE    MYNEWT_VAL(OC_MAX_PAYLOAD_SIZE)
 
 /* Number of devices on the OCF platform */
-#define MAX_NUM_DEVICES (1)
+#define MAX_NUM_DEVICES     MYNEWT_VAL(OC_NUM_DEVICES)
 
 /* Platform payload size */
-#define MAX_PLATFORM_PAYLOAD_SIZE (256)
+#define MAX_PLATFORM_PAYLOAD_SIZE MYNEWT_VAL(OC_MAX_PAYLOAD)
 
 /* Device payload size */
-#define MAX_DEVICE_PAYLOAD_SIZE (256)
+#define MAX_DEVICE_PAYLOAD_SIZE   MYNEWT_VAL(OC_MAX_PAYLOAD)
 
 /* Security layer */
 /* Maximum number of authorized clients */
