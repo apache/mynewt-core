3662f2628d7216e7c0f0ee051bd24e78151f621b
Marko Kiiskila <marko@runtime.io>
2016-11-03 16:09:59 -0700

    MYNEWT-418; nrf51/nrf52 bsps to use common download/debug functions.

--- hw/bsp/nrf51-arduino_101/nrf51dk-16kbram_debug.sh
+++ hw/bsp/nrf51-arduino_101/nrf51dk-16kbram_debug.sh
@@ -18,34 +18,19 @@
 #
 
 # Called with following variables set:
+#  - CORE_PATH is absolute path to @apache-mynewt-core
 #  - BSP_PATH is absolute path to hw/bsp/bsp_name
 #  - BIN_BASENAME is the path to prefix to target binary,
 #    .elf appended to name is the ELF file
 #  - FEATURES holds the target features string
 #  - EXTRA_JTAG_CMD holds extra parameters to pass to jtag software
 #  - RESET set if target should be reset when attaching
+#  - NO_GDB set if we should not start gdb to debug
 #
-if [ -z "$BIN_BASENAME" ]; then
-    echo "Need binary to debug"
-    exit 1
-fi
+
+. $CORE_PATH/hw/scripts/jlink.sh
 
 FILE_NAME=$BIN_BASENAME.elf
-GDB_CMD_FILE=.gdb_cmds
+JLINK_DEV="nRF51422_xxAC"
 
-echo "Debugging" $FILE_NAME
-
-set -m
-JLinkGDBServer -device nRF51422_xxAC -speed 4000 -if SWD -port 3333 -singlerun > /dev/null &
-set +m
-
-echo "target remote localhost:3333" > $GDB_CMD_FILE
-
-# Whether target should be reset or not
-if [ ! -z "$RESET" ]; then
-    echo "mon reset" >> $GDB_CMD_FILE
-fi
-
-arm-none-eabi-gdb --tui -x $GDB_CMD_FILE $FILE_NAME
-
-rm $GDB_CMD_FILE
+jlink_debug
