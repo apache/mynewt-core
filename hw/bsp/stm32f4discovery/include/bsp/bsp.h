09fd6e4d0ac155aa13c38edb5ea7fd2070fbaf7e
Fabio Utzig <utzig@utzig.org>
2016-11-28 09:52:59 -0200

    Use correct pin for LED

--- hw/bsp/stm32f4discovery/include/bsp/bsp.h
+++ hw/bsp/stm32f4discovery/include/bsp/bsp.h
@@ -41,7 +41,7 @@ extern uint8_t _ccram_start;
 #define CCRAM_SIZE      (64 * 1024)
 
 /* LED pins */
-#define LED_BLINK_PIN   MCU_GPIO_PORTD(12)
+#define LED_BLINK_PIN   MCU_GPIO_PORTD(13)
 
 /* UART */
 #define UART_CNT 1
