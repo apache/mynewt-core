fded8a353024770f591b39bb4508bb9fafc99352
William San Filippo <wills@runtime.io>
2017-01-09 16:27:16 -0800

    No jira ticket for this commit. The nordic startup code was not zeroing BSS. We now zero the BSS in the gcc_startup_nrfxx.s file to insure that it is zeroed. This was not an issue previously as the c startup code was doing this.

--- hw/bsp/nrf51dk-16kbram/src/arch/cortex_m0/gcc_startup_nrf51.s
+++ hw/bsp/nrf51dk-16kbram/src/arch/cortex_m0/gcc_startup_nrf51.s
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright (c) 2015, Nordic Semiconductor ASA
 All rights reserved.
 
@@ -149,13 +149,23 @@ Reset_Handler:
     ORRS    R2, R1
     STR     R2, [R0]
 
+    /* Clear BSS */
+    subs    r0, r0
+    ldr     r2, =__bss_start__
+    ldr     r3, =__bss_end__
+.bss_zero_loop:
+    cmp     r2, r3
+    bhs     .data_copy_loop
+    stmia   r2!, {r0}
+    b    .bss_zero_loop
+
 /*     Loop to copy data from read only memory to RAM. The ranges
  *      of copy from/to are specified by following symbols evaluated in
  *      linker script.
  *      __etext: End of code section, i.e., begin of data sections to copy from.
  *      __data_start__/__data_end__: RAM address range that data should be
  *      copied to. Both must be aligned to 4 bytes boundary.  */
-
+.data_copy_loop:
     ldr    r1, =__etext
     ldr    r2, =__data_start__
     ldr    r3, =__data_end__
