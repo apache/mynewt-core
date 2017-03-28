5445c885d6cbad9b2d4ba7d284f55462d15a724a
Michał Narajowski <michal.narajowski@codecoup.pl>
2017-03-28 16:35:48 +0200

    sys/console: use timer instead of task for rtt polling

--- sys/console/full/src/rtt_console.c
+++ sys/console/full/src/rtt_console.c
@@ -23,15 +23,13 @@
 #include <ctype.h>
 
 #include "os/os.h"
+#include "os/os_cputime.h"
 #include "rtt/SEGGER_RTT.h"
 #include "console/console.h"
 #include "console_priv.h"
 
 #if MYNEWT_VAL(CONSOLE_INPUT)
-#define RTT_TASK_PRIO         (5)
-#define RTT_STACK_SIZE        (512)
-static struct os_task rtt_task;
-static os_stack_t rtt_task_stack[RTT_STACK_SIZE];
+static struct hal_timer rtt_timer;
 #endif
 
 static const char CR = '\r';
@@ -57,47 +55,31 @@ console_out(int character)
 void
 rtt(void *arg)
 {
-    int sr;
     int key;
     int i = 0;
+    uint32_t timeout;
 
-    while (1) {
-        OS_ENTER_CRITICAL(sr);
-        key = SEGGER_RTT_GetKey();
-        if (key >= 0) {
-            console_handle_char((char)key);
-            i = 0;
-        }
-        OS_EXIT_CRITICAL(sr);
-        /* These values were selected to keep the shell responsive
-         * and at the same time reduce context switches.
-         * Min sleep is 50ms and max is 250ms.
-         */
-        if (i < 5) {
-            ++i;
-        }
-        os_time_delay((OS_TICKS_PER_SEC / 20) * i);
+    key = SEGGER_RTT_GetKey();
+    if (key >= 0) {
+        console_handle_char((char)key);
+        i = 0;
     }
-}
-
-static void
-init_task(void)
-{
-    /* Initialize the task */
-    os_task_init(&rtt_task, "rtt", rtt, NULL, RTT_TASK_PRIO,
-                 OS_WAIT_FOREVER, rtt_task_stack, RTT_STACK_SIZE);
+    /* These values were selected to keep the shell responsive
+     * and at the same time reduce context switches.
+     * Min sleep is 50ms and max is 250ms.
+     */
+    if (i < 5) {
+        ++i;
+    }
+    timeout = 50000 * i;
+    os_cputime_timer_relative(&rtt_timer, timeout);
 }
 #endif
 
 int
 rtt_console_is_init(void)
 {
-#if MYNEWT_VAL(CONSOLE_INPUT)
-    return rtt_task.t_state == OS_TASK_READY ||
-           rtt_task.t_state == OS_TASK_SLEEP;
-#else
     return 1;
-#endif
 }
 
 int
@@ -105,7 +87,9 @@ rtt_console_init(void)
 {
     SEGGER_RTT_Init();
 #if MYNEWT_VAL(CONSOLE_INPUT)
-    init_task();
+    os_cputime_timer_init(&rtt_timer, rtt, NULL);
+    /* start after a second */
+    os_cputime_timer_relative(&rtt_timer, 1000000);
 #endif
     return 0;
 }
