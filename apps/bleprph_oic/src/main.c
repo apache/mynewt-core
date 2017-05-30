79fcf1d2a81d3487f237d696c69d6a904bb94ec5
Christopher Collins <ccollins@apache.org>
2017-05-30 11:21:39 -0700

    MYNEWT-750 Handle repeat pairing in e.g. apps.

--- apps/bleprph_oic/src/main.c
+++ apps/bleprph_oic/src/main.c
@@ -232,6 +232,22 @@ bleprph_gap_event(struct ble_gap_event *event, void *arg)
                     event->mtu.channel_id,
                     event->mtu.value);
         return 0;
+
+    case BLE_GAP_EVENT_REPEAT_PAIRING:
+        /* We already have a bond with the peer, but it is attempting to
+         * establish a new secure link.  This app sacrifices security for
+         * convenience: just throw away the old bond and accept the new link.
+         */
+
+        /* Delete the old bond. */
+        rc = ble_gap_conn_find(event->repeat_pairing.conn_handle, &desc);
+        assert(rc == 0);
+        ble_store_util_delete_peer(&desc.peer_id_addr);
+
+        /* Return BLE_GAP_REPEAT_PAIRING_RETRY to indicate that the host should
+         * continue with the pairing operation.
+         */
+        return BLE_GAP_REPEAT_PAIRING_RETRY;
     }
 
     return 0;
