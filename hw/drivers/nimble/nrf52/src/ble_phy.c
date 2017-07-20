3a24cfb3e0c4cf00e91966a4a549b05acd006d8c
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-07-20 13:16:57 +0200

    nimble/controller: Apply wfr workaround to Coded PHY only
    We still need to fix this, but at least make this magic workaround be
    applied only for LE Coded since there's no need for it on 1M and 2M.

--- hw/drivers/nimble/nrf52/src/ble_phy.c
+++ hw/drivers/nimble/nrf52/src/ble_phy.c
@@ -462,13 +462,18 @@ ble_phy_wfr_enable(int txrx, uint32_t wfr_usecs)
         end_time = NRF_TIMER0->CC[2] + BLE_LL_IFS +
             ble_phy_mode_pdu_start_off(phy) + BLE_LL_JITTER_USECS;
 
-        /*
-         * FIXME!
-         * on Coded PHY the time calculated above seems to be too short - adding
-         * extra 32us "solves" the problem (extra 16us, i.e. doubling the jitter
-         * is not enough). Need to figure out what is wrong here.
-         */
-        end_time += 32;
+#if (BLE_LL_BT5_PHY_SUPPORTED == 1)
+        if (phy == BLE_PHY_MODE_CODED_125KBPS ||
+                phy == BLE_PHY_MODE_CODED_500KBPS) {
+            /*
+             * FIXME!
+             * on Coded PHY the time calculated above seems to be too short
+             * adding extra 32us "solves" the problem (extra 16us, i.e. doubling
+             * the jitter is not enough). Need to figure out what is wrong here.
+             */
+            end_time += 32;
+        }
+#endif
     } else {
         /* CC[0] is set to when RXEN occurs. NOTE: the extra 16 usecs is
            jitter */
