f82a8561ffeea1c246d414fa5a152be0ec4817b0
Miguel Azevedo <miguellazev@gmail.com>
2017-08-07 13:21:37 +0000

    Style improvements, creating devs on hal_bsp_init(), syscfg/syscfg.h included befora other #include's

--- apps/pwm_test/src/main.c
+++ apps/pwm_test/src/main.c
@@ -16,49 +16,46 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+#include "syscfg/syscfg.h"
 #include "sysinit/sysinit.h"
 #include <os/os.h>
 #include <pwm/pwm.h>
 #include <pwm_nrf52/pwm_nrf52.h>
 #include <bsp/bsp.h>
 
-static struct os_dev dev;
-int arg = 0;
 struct pwm_dev *pwm;
 static int value = 10000;
 
 int
 main(int argc, char **argv)
 {
-    sysinit();
-
     struct nrf52_pwm_chan_cfg chan_conf = {
         .pin = LED_1,
         .inverted = true
     };
 
-    os_dev_create(&dev,
-                  "pwm",
-                  OS_DEV_INIT_KERNEL,
-                  OS_DEV_INIT_PRIO_DEFAULT,
-                  nrf52_pwm_dev_init,
-                  NULL);
-    pwm = (struct pwm_dev *) os_dev_open("pwm", 0, NULL);
+    sysinit();
 
+    pwm = (struct pwm_dev *) os_dev_open("pwm0", 0, NULL);
+
+    /* setup led 1 */
     pwm_chan_config(pwm, 0, &chan_conf);
     pwm_enable_duty_cycle(pwm, 0, 10000);
 
+    /* setup led 2 */
     chan_conf.pin = LED_2;
     pwm_chan_config(pwm, 1, &chan_conf);
     pwm_enable_duty_cycle(pwm, 1, value/10);
 
-    //changing frequency while playing
+    /* change frequency while playing */
     pwm_set_frequency(pwm, 2000000);
 
+    /* setup led 3 */
     chan_conf.pin = LED_3;
     pwm_chan_config(pwm, 2, &chan_conf);
     pwm_enable_duty_cycle(pwm, 2, value/100);
 
+    /* setup led 4 */
     chan_conf.pin = LED_4;
     pwm_chan_config(pwm, 3, &chan_conf);
     pwm_enable_duty_cycle(pwm, 3, value/1000);
