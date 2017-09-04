99b8df71b8d6529a510a7adb1a2b1c0949dacdb0
Francois Berder <fberder@outlook.fr>
2017-09-04 10:26:29 +0100

    hw: bsp: pic32mz2048_wi-fire: Define buttons
    Signed-off-by: Francois Berder <fberder@outlook.fr>

--- hw/bsp/pic32mz2048_wi-fire/include/bsp/bsp.h
+++ hw/bsp/pic32mz2048_wi-fire/include/bsp/bsp.h
@@ -47,6 +47,10 @@ extern uint8_t _ccram_start;
 #define LED_4           MCU_GPIO_PORTG(15)
 #define LED_BLINK_PIN   LED_1
 
+/* Buttons */
+#define BUTTON_1        MCU_GPIO_PORTA(5)
+#define BUTTON_2        MCU_GPIO_PORTA(4)
+
 /* UART */
 #define UART_CNT        (6)
 
