09af9bdcf80e0d493180a1e3045402cfb48f55aa
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-10-09 11:13:36 +0200

    nimble/ll: Cleanup aux scheduling function
    Cleanup variable names and comments to make it logical, i.e. there's no
    "earliest start" here since we either can schedule aux scan at specific
    point of time or not - we can't adjust it.

--- net/nimble/controller/src/ble_ll_sched.c
+++ net/nimble/controller/src/ble_ll_sched.c
@@ -1341,8 +1341,8 @@ ble_ll_sched_aux_scan(struct ble_mbuf_hdr *ble_hdr,
 {
     int rc;
     os_sr_t sr;
-    uint32_t earliest_start;
-    uint32_t earliest_end;
+    uint32_t start_time;
+    uint32_t end_time;
     uint32_t dur;
     uint32_t now;
     struct ble_ll_sched_item *entry;
@@ -1351,9 +1351,9 @@ ble_ll_sched_aux_scan(struct ble_mbuf_hdr *ble_hdr,
 
     sch = &aux_scan->sch;
 
-    now =  ble_hdr->beg_cputime;
-    earliest_start = now + os_cputime_usecs_to_ticks(aux_scan->offset);
-    earliest_start -= g_ble_ll_sched_offset_ticks;
+    now = ble_hdr->beg_cputime;
+    start_time = now + os_cputime_usecs_to_ticks(aux_scan->offset);
+    start_time -= g_ble_ll_sched_offset_ticks;
 
     /* Let's calculate time we reserve for aux packet. For now we assume to wait
      * for fixed number of bytes and handle possible interrupting it in
@@ -1363,14 +1363,12 @@ ble_ll_sched_aux_scan(struct ble_mbuf_hdr *ble_hdr,
     phy_mode = ble_ll_phy_to_phy_mode(aux_scan->aux_phy,
                                       BLE_HCI_LE_PHY_CODED_ANY);
     dur = ble_ll_pdu_tx_time_get(BLE_LL_SCHED_AUX_PTR_DFLT_BYTES_NUM, phy_mode);
-    earliest_end = earliest_start + os_cputime_usecs_to_ticks(dur);
+    end_time = start_time + os_cputime_usecs_to_ticks(dur);
 
-    /* We have to find a place for this schedule */
+    sch->start_time = start_time;
+    sch->end_time = end_time;
+
     OS_ENTER_CRITICAL(sr);
-
-    /* The schedule item must occur after current running item (if any) */
-    sch->start_time = earliest_start;
-    sch->end_time = earliest_end;
 
     if (!ble_ll_sched_insert_if_empty(sch)) {
         /* Nothing in schedule. Schedule as soon as possible
@@ -1382,10 +1380,6 @@ ble_ll_sched_aux_scan(struct ble_mbuf_hdr *ble_hdr,
     /* Try to find slot for aux scan. */
     os_cputime_timer_stop(&g_ble_ll_sched_timer);
     TAILQ_FOREACH(entry, &g_ble_ll_sched_q, link) {
-        /* Set these because overlap function needs them to be set */
-        sch->start_time = earliest_start;
-        sch->end_time = earliest_end;
-
         /* We can insert if before entry in list */
         if (sch->end_time <= entry->start_time) {
             rc = 0;
