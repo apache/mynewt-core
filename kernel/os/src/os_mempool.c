apache/mynewt-core

commit abd8c4598b9a618a971a0281f6e767918a1bf474

os; 2 syscfg variables for debugging memory pool use. Poison freed memory on free, check the pattern on re-alloc. Check for dup frees and validate address of memory block put back to pool.

kernel/os/src/os_mempool.c
@@ -29,14 +29,44 @@
  *   @defgroup OSMempool Memory Pools
  *   @{
  */
-/*
-#define OS_CHECK_DUP_FREE 1
-*/
 
-#define OS_MEMPOOL_TRUE_BLOCK_SIZE(bsize)   OS_ALIGN(bsize, OS_ALIGNMENT)
+#define OS_MEM_TRUE_BLOCK_SIZE(bsize)   OS_ALIGN(bsize, OS_ALIGNMENT)
+#define OS_MEMPOOL_TRUE_BLOCK_SIZE(mp) OS_MEM_TRUE_BLOCK_SIZE(mp->mp_block_size)
 
 STAILQ_HEAD(, os_mempool) g_os_mempool_list =
     STAILQ_HEAD_INITIALIZER(g_os_mempool_list);
+
+#if MYNEWT_VAL(OS_MEMPOOL_POISON)
+static uint32_t os_mem_poison = 0xde7ec7ed;
+
+static void
+os_mempool_poison(void *start, int sz)
+{
+    int i;
+    char *p = start;
+
+    for (i = sizeof(struct os_memblock); i < sz;
+         i = i + sizeof(os_mem_poison)) {
+        memcpy(p + i, &os_mem_poison, min(sizeof(os_mem_poison), sz - i));
+    }
+}
+
+static void
+os_mempool_poison_check(void *start, int sz)
+{
+    int i;
+    char *p = start;
+
+    for (i = sizeof(struct os_memblock); i < sz;
+         i = i + sizeof(os_mem_poison)) {
+        assert(!memcmp(p + i, &os_mem_poison,
+               min(sizeof(os_mem_poison), sz - i)));
+    }
+}
+#else
+#define os_mempool_poison(start, sz)
+#define os_mempool_poison_check(start, sz)
+#endif
 
 /**
  * os mempool init
@@ -60,7 +90,7 @@ os_mempool_init(struct os_mempool *mp, int blocks, int block_size,
     struct os_memblock *block_ptr;
 
     /* Check for valid parameters */
-    if ((!mp) || (blocks < 0) || (block_size <= 0)) {
+    if (!mp || (blocks < 0) || (block_size <= sizeof(struct os_memblock))) {
         return OS_INVALID_PARM;
     }
 
@@ -76,7 +106,7 @@ os_mempool_init(struct os_mempool *mp, int blocks, int block_size,
             return OS_MEM_NOT_ALIGNED;
         }
     }
-    true_block_size = OS_MEMPOOL_TRUE_BLOCK_SIZE(block_size);
+    true_block_size = OS_MEM_TRUE_BLOCK_SIZE(block_size);
 
     /* Initialize the memory pool structure */
     mp->mp_block_size = block_size;
@@ -85,6 +115,7 @@ os_mempool_init(struct os_mempool *mp, int blocks, int block_size,
     mp->mp_num_blocks = blocks;
     mp->mp_membuf_addr = (uint32_t)membuf;
     mp->name = name;
+    os_mempool_poison(membuf, true_block_size);
     SLIST_FIRST(mp) = membuf;
 
     /* Chain the memory blocks to the free list */
@@ -92,6 +123,7 @@ os_mempool_init(struct os_mempool *mp, int blocks, int block_size,
     block_ptr = (struct os_memblock *)block_addr;
     while (blocks > 1) {
         block_addr += true_block_size;
+        os_mempool_poison(block_addr, true_block_size);
         SLIST_NEXT(block_ptr, mb_next) = (struct os_memblock *)block_addr;
         block_ptr = (struct os_memblock *)block_addr;
         --blocks;
@@ -125,6 +157,7 @@ os_mempool_is_sane(const struct os_mempool *mp)
         if (!os_memblock_from(mp, block)) {
             return false;
         }
+        os_mempool_poison_check(block, OS_MEMPOOL_TRUE_BLOCK_SIZE(mp));
     }
 
     return true;
@@ -150,7 +183,7 @@ os_memblock_from(const struct os_mempool *mp, const void *block_addr)
                    "Pointer to void must be 32-bits.");
 
     baddr32 = (uint32_t)block_addr;
-    true_block_size = OS_MEMPOOL_TRUE_BLOCK_SIZE(mp->mp_block_size);
+    true_block_size = OS_MEMPOOL_TRUE_BLOCK_SIZE(mp);
     end = mp->mp_membuf_addr + (mp->mp_num_blocks * true_block_size);
 
     /* Check that the block is in the memory buffer range. */
@@ -190,6 +223,8 @@ os_memblock_get(struct os_mempool *mp)
             /* Get a free block */
             block = SLIST_FIRST(mp);
 
+            os_mempool_poison_check(block, OS_MEMPOOL_TRUE_BLOCK_SIZE(mp));
+
             /* Set new free list head */
             SLIST_FIRST(mp) = SLIST_NEXT(block, mb_next);
 
@@ -226,16 +261,18 @@ os_memblock_put(struct os_mempool *mp, void *block_addr)
         return OS_INVALID_PARM;
     }
 
+#if MYNEWT_VAL(OS_MEMPOOL_CHECK)
     /* Check that the block we are freeing is a valid block! */
-    if (!os_memblock_from(mp, block_addr)) {
-        return OS_INVALID_PARM;
-    }
+    assert(os_memblock_from(mp, block_addr));
 
-#ifdef OS_CHECK_DUP_FREE
+    /*
+     * Check for duplicate free.
+     */
     SLIST_FOREACH(block, mp, mb_next) {
         assert(block != (struct os_memblock *)block_addr);
     }
 #endif
+    os_mempool_poison(block_addr, OS_MEMPOOL_TRUE_BLOCK_SIZE(mp));
     block = (struct os_memblock *)block_addr;
     OS_ENTER_CRITICAL(sr);
 

kernel/os/include/os/os.h
@@ -23,6 +23,8 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+#include <syscfg/syscfg.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif

kernel/os/syscfg.yml
@@ -41,10 +41,16 @@ syscfg.defs:
         value: 1
     OS_CTX_SW_STACK_CHECK:
         description: 'Whether to do stack sanity check during context switch'
-        value: 1
+        value: 0
     OS_CTX_SW_STACK_GUARD:
         description: 'How many os_stack_ts to keep as stack guard'
         value: 4
+    OS_MEMPOOL_CHECK:
+        description: 'Whether to do stack sanity check of mempool operations'
+        value: 0
+    OS_MEMPOOL_POISON:
+        description: 'Whether to do write known pattern to freed memory'
+        value: 0
     OS_CPUTIME_FREQ:
         description: 'Frequency of os cputime'
         value: 1000000

kernel/os/test/src/testcases/os_mempool_test_case.c
@@ -6,7 +6,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -19,11 +19,11 @@
 #include "os_test_priv.h"
 
 /**
- * os mempool test 
- *  
- * Main test loop for memory pool testing. 
- * 
- * @return int 
+ * os mempool test
+ *
+ * Main test loop for memory pool testing.
+ *
+ * @return int
  */
 void
 mempool_test(int num_blocks, int block_size)
@@ -31,7 +31,6 @@ mempool_test(int num_blocks, int block_size)
     int cnt;
     int true_block_size;
     int mem_pool_size;
-    uint32_t test_block;
     uint8_t *tstptr;
     void **free_ptr;
     void *block;
@@ -40,7 +39,7 @@ mempool_test(int num_blocks, int block_size)
     /* Check for too many blocks */
     TEST_ASSERT(num_blocks <= MEMPOOL_TEST_MAX_BLOCKS);
 
-    rc = os_mempool_init(&g_TstMempool, num_blocks, MEM_BLOCK_SIZE, 
+    rc = os_mempool_init(&g_TstMempool, num_blocks, MEM_BLOCK_SIZE,
                          &TstMembuf[0], "TestMemPool");
     TEST_ASSERT_FATAL(rc == 0, "Error creating memory pool %d", rc);
 
@@ -125,7 +124,7 @@ mempool_test(int num_blocks, int block_size)
         }
     }
 
-    TEST_ASSERT((cnt == g_TstMempool.mp_num_blocks) && 
+    TEST_ASSERT((cnt == g_TstMempool.mp_num_blocks) &&
                 (cnt != MEMPOOL_TEST_MAX_BLOCKS),
                 "Got more blocks than mempool contains (%d vs %d)",
                 cnt, g_TstMempool.mp_num_blocks);
@@ -139,7 +138,7 @@ mempool_test(int num_blocks, int block_size)
     for (cnt = 0; cnt < g_TstMempool.mp_num_blocks; ++cnt) {
         rc = os_memblock_put(&g_TstMempool, block_array[cnt]);
         TEST_ASSERT(rc == 0,
-                    "Error putting back block %p (cnt=%d err=%d)", 
+                    "Error putting back block %p (cnt=%d err=%d)",
                     block_array[cnt], cnt, rc);
     }
 
@@ -158,21 +157,6 @@ mempool_test(int num_blocks, int block_size)
     TEST_ASSERT(os_memblock_get(NULL) == NULL,
                 "No error trying to get a block from NULL pool");
 
-    /* Attempt to free a block outside the range of the membuf */
-    test_block = g_TstMempool.mp_membuf_addr;
-    test_block -= 4;
-    rc = os_memblock_put(&g_TstMempool, (void *)test_block);
-    TEST_ASSERT(rc == OS_INVALID_PARM, "No error freeing bad block address");
-
-    test_block += (true_block_size * g_TstMempool.mp_num_blocks) + 100;
-    rc = os_memblock_put(&g_TstMempool, (void *)test_block);
-    TEST_ASSERT(rc == OS_INVALID_PARM, "No error freeing bad block address");
-
-    /* Attempt to free on bad boundary */
-    test_block = g_TstMempool.mp_membuf_addr;
-    test_block += (true_block_size / 2);
-    rc = os_memblock_put(&g_TstMempool, (void *)test_block);
-    TEST_ASSERT(rc == OS_INVALID_PARM, "No error freeing bad block address");
 }
 
 TEST_CASE(os_mempool_test_case)

commit 0fd8239b7f914ddae77ac396bfce288fd6bc5b5f

kernel/os_mempool: Fix for incorrect check on os_mempool_init
This is regression from abd8c4598b as it was changing API incorretly.
Documentation says that 'block_size' parameter in os_mempool_init() is
"Minimum size of an individual element in pool"

It does not required from the user to take into account of 'block_size'
an internal overhead which is sizeof(struct os_memblock).

The only requirement on user is to make sure that provided 'membuf' has
enough bytes which can be calculated using OS_MEMPOOL_BYTES() macro.

Without this patch we have assert when creating mempool with block_size=4

kernel/os/src/os_mempool.c
@@ -90,7 +90,7 @@ os_mempool_init(struct os_mempool *mp, int blocks, int block_size,
     struct os_memblock *block_ptr;
 
     /* Check for valid parameters */
-    if (!mp || (blocks < 0) || (block_size <= sizeof(struct os_memblock))) {
+    if (!mp || (blocks < 0) || (block_size <= 0)) {
         return OS_INVALID_PARM;
     }
 