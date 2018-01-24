25b88a04a2f60e60669d1e167046884be6d8605d
Sterling Hughes <sterling@runtime.io>
2018-01-24 13:34:47 -0800

    update other F4 bsps to new STCube, and ensure smoke test works.

--- hw/bsp/olimex_stm32-e407_devboard/src/system_stm32f4xx.c
+++ hw/bsp/olimex_stm32-e407_devboard/src/system_stm32f4xx.c
@@ -109,6 +109,7 @@
 uint32_t SystemCoreClock = 168000000;
 
 const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+const uint8_t APBPrescTable[8] = {0U, 0U, 0U, 0U, 1U, 2U, 3U, 4U};
 
 /**
   * @}
