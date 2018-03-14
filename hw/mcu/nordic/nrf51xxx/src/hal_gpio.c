18b3444998a0dded3c6f09deefd547188f8a077b
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-03-14 11:01:13 +0100

    hw/mcu/nordic: Fire GPIO handler only if event is enabled
    We could fire callback for GPIO even if it should be disabled:
    - configure pins A and B for interrupt
    - enable pin A, disable pin B
    - toggle pin B
    - toggle pin A
    
    callbacks for both A and B will be fired since EVENT_IN bit for pin B is
    set and we do not check if interrupt is actually enabled for this one.

--- hw/mcu/nordic/nrf51xxx/src/hal_gpio.c
+++ hw/mcu/nordic/nrf51xxx/src/hal_gpio.c
@@ -165,7 +165,7 @@ hal_gpio_irq_handler(void)
     int i;
 
     for (i = 0; i < HAL_GPIO_MAX_IRQ; i++) {
-        if (NRF_GPIOTE->EVENTS_IN[i]) {
+        if (NRF_GPIOTE->EVENTS_IN[i] && (NRF_GPIOTE->INTENSET & (1 << i))) {
             NRF_GPIOTE->EVENTS_IN[i] = 0;
             if (hal_gpio_irqs[i].func) {
                 hal_gpio_irqs[i].func(hal_gpio_irqs[i].arg);
