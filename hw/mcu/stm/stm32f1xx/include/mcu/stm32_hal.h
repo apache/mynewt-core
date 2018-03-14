2f3da40704d65351882fa88720aa8f7b91dd2a55
Fabio Utzig <utzig@apache.org>
2018-03-14 17:17:34 -0300

    Add F1 SPI config and fix build
    - f1xx brokage fixed due to not Alternate in GPIO
    - typos

--- hw/mcu/stm/stm32f1xx/include/mcu/stm32_hal.h
+++ hw/mcu/stm/stm32f1xx/include/mcu/stm32_hal.h
@@ -29,6 +29,8 @@ extern "C" {
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_def.h"
 
+#include "stm32f1xx_mynewt_hal.h"
+
 /* hal_watchdog */
 #include "stm32f1xx_hal_iwdg.h"
 #define STM32_HAL_WATCHDOG_CUSTOM_INIT(x)
