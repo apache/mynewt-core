34859b5ece676a4d618cd269d3121341fc80e0cf
Christopher Collins <ccollins@apache.org>
2018-03-27 13:57:46 -0700

    stm32f1 - Add missing prototype.

--- hw/mcu/stm/stm32f1xx/include/mcu/stm32f1_bsp.h
+++ hw/mcu/stm/stm32f1xx/include/mcu/stm32f1_bsp.h
@@ -42,6 +42,12 @@ struct stm32f1_uart_cfg {
     IRQn_Type suc_irqn;                 /* NVIC IRQn */
 };
 
+/*
+ * Internal API for stm32f1xx mcu specific code.
+ */
+int hal_gpio_init_af(int pin, uint8_t af_type, enum hal_gpio_pull pull,
+                     uint8_t od);
+
 struct hal_flash;
 extern struct hal_flash stm32f1_flash_dev;
 
