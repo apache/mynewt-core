2bb24c0f86120ee97034b1b9252d85b5ff5c4ded
Fabio Utzig <utzig@apache.org>
2018-04-26 19:55:27 -0300

    Move some timer configs inside the driver
    STM32_OFLOW_VALUE/STM32_NSEC_PER_SEC have the same value for all timers
    use in all MCU models so removing them from the configurable hal header.

--- hw/mcu/stm/stm32f4xx/include/mcu/stm32_hal.h
+++ hw/mcu/stm/stm32f4xx/include/mcu/stm32_hal.h
@@ -70,8 +70,6 @@ struct stm32_hal_spi_cfg {
 #include "stm32f4xx_hal_tim.h"
 
 #define STM32_HAL_TIMER_MAX     (3)
-#define STM32_OFLOW_VALUE       (0x10000UL)
-#define STM32_NSEC_PER_SEC      (1000000000UL)
 
 #ifdef __cplusplus
 }
