3e8c92cc3752c09a3a8da3ce5c91555723b52d20
Kevin Townsend <contact@microbuilder.eu>
2018-07-11 09:02:20 +0200

    Added sim driver registration and function redirection

--- hw/mcu/native/src/hal_i2c.c
+++ hw/mcu/native/src/hal_i2c.c
@@ -18,11 +18,20 @@
  */
 
 #include <stdio.h>
-#include <hal/hal_i2c.h>
+#include "os/mynewt.h"
+#include "hal/hal_i2c.h"
+#include "hal/hal_i2c_sim.h"
+
+struct {
+    struct os_mutex mgr_lock;
+    SLIST_HEAD(, hal_i2c_sim_driver) mgr_sim_list;
+} hal_i2c_sim_mgr;
 
 int
 hal_i2c_init(uint8_t i2c_num, void *cfg)
 {
+    os_mutex_init(&hal_i2c_sim_mgr.mgr_lock);
+
     return 0;
 }
 
@@ -30,15 +39,16 @@ int
 hal_i2c_master_write(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
                      uint32_t timeout, uint8_t last_op)
 {
-    uint16_t i;
+    struct hal_i2c_sim_driver *cursor;
 
-    printf("hal_i2c wrote %d byte(s) on device 0x%02X (I2C_%d):",
-           pdata->len, pdata->address, i2c_num);
-    for (i=0; i<pdata->len; i++) {
-        printf(" %02X", pdata->buffer[i]);
+    cursor = NULL;
+    SLIST_FOREACH(cursor, &hal_i2c_sim_mgr.mgr_sim_list, s_next) {
+        if (cursor->addr == pdata->address) {
+            /* Forward the read request to the sim driver */
+            return cursor->sd_write(i2c_num, pdata, timeout, last_op);
+        }
     }
-    printf("\n");
-    fflush(stdout);
+
     return 0;
 }
 
@@ -46,15 +56,15 @@ int
 hal_i2c_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
                     uint32_t timeout, uint8_t last_op)
 {
-    uint16_t i;
+    struct hal_i2c_sim_driver *cursor;
 
-    printf("hal_i2c read  %d byte(s) on device 0x%02X (I2C_%d):",
-           pdata->len, pdata->address, i2c_num);
-    for (i=0; i<pdata->len; i++) {
-        printf(" %02X", pdata->buffer[i]);
+    cursor = NULL;
+    SLIST_FOREACH(cursor, &hal_i2c_sim_mgr.mgr_sim_list, s_next) {
+        if (cursor->addr == pdata->address) {
+            /* Forward the read request to the sim driver */
+            return cursor->sd_read(i2c_num, pdata, timeout, last_op);
+        }
     }
-    printf("\n");
-    fflush(stdout);
 
     return 0;
 }
@@ -63,13 +73,79 @@ int
 hal_i2c_master_probe(uint8_t i2c_num, uint8_t address,
                      uint32_t timeout)
 {
-    int val;
+    struct hal_i2c_sim_driver *cursor;
 
-    /* ToDo: return '0' on any I2C address(es) you wish to match */
-    switch (address) {
-        default:
-          val = -1;
+    cursor = NULL;
+    SLIST_FOREACH(cursor, &hal_i2c_sim_mgr.mgr_sim_list, s_next) {
+        if (cursor->addr == address) {
+            return 0;
+        }
     }
 
-    return val;
+    return -1;
+}
+
+/**
+ * Lock sim manager to access the list of simulated drivers
+ */
+int
+hal_i2c_sim_mgr_lock(void)
+{
+    int rc;
+
+    rc = os_mutex_pend(&hal_i2c_sim_mgr.mgr_lock, OS_TIMEOUT_NEVER);
+    if (rc == 0 || rc == OS_NOT_STARTED) {
+        return (0);
+    }
+    return (rc);
+}
+
+/**
+ * Unlock sim manager once the list of simulated drivers has been accessed
+ */
+void
+hal_i2c_sim_mgr_unlock(void)
+{
+    (void) os_mutex_release(&hal_i2c_sim_mgr.mgr_lock);
+}
+
+/**
+ * Insert a simulated driver into the list
+ */
+static void
+hal_i2c_sim_mgr_insert(struct hal_i2c_sim_driver *driver)
+{
+    struct hal_i2c_sim_driver *cursor, *prev;
+
+    prev = cursor = NULL;
+    SLIST_FOREACH(cursor, &hal_i2c_sim_mgr.mgr_sim_list, s_next) {
+        prev = cursor;
+    }
+
+    if (prev == NULL) {
+        SLIST_INSERT_HEAD(&hal_i2c_sim_mgr.mgr_sim_list, driver, s_next);
+    } else {
+        SLIST_INSERT_AFTER(prev, driver, s_next);
+    }
+}
+
+int
+hal_i2c_sim_register(struct hal_i2c_sim_driver *drv)
+{
+    int rc;
+
+    rc = hal_i2c_sim_mgr_lock();
+    if (rc != 0) {
+        goto err;
+    }
+
+    printf("Registering I2C sim driver for 0x%02X\n", drv->addr);
+    fflush(stdout);
+    hal_i2c_sim_mgr_insert(drv);
+
+    hal_i2c_sim_mgr_unlock();
+
+    return (0);
+err:
+    return (rc);
 }
