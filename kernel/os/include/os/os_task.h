4d5f9e62fadefdcb53b43e50e75d9ac185ac86b5
Christopher Collins <ccollins@apache.org>
2018-08-10 17:37:09 -0700

    kernel/os: Allow stacks without "static"
    This commit introduces a new macro: OS_TASK_STACK_DEFINE_NOSTATIC. This
    macro defines a task stack, but without a leading "static" and trailing
    ";".
    
    This macro is needed to to create an array of stacks.

--- kernel/os/include/os/os_task.h
+++ kernel/os/include/os/os_task.h
@@ -37,9 +37,12 @@
 extern "C" {
 #endif
 
+#define OS_TASK_STACK_DEFINE_NOSTATIC(__name, __size) \
+    os_stack_t __name [OS_STACK_ALIGN(__size)] \
+    __attribute__((aligned(OS_STACK_ALIGNMENT)))
+
 #define OS_TASK_STACK_DEFINE(__name, __size) \
-    static os_stack_t __name [OS_STACK_ALIGN(__size)] \
-        __attribute__((aligned(OS_STACK_ALIGNMENT)));
+    static OS_TASK_STACK_DEFINE_NOSTATIC(__name, __size);
 
 /** Highest priority task */
 #define OS_TASK_PRI_HIGHEST (0)
