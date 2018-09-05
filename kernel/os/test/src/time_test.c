cfc5856429ed3ff5bad59fae5138f431f7c97bf2
Christopher Collins <ccollins@apache.org>
2018-09-04 18:31:53 -0700

    kernel/os: Registration of time-change listeners
    This commit enables the registration of time-change listeners.  When the
    time is set via a call to `os_settimeofday()`, all registered listeners
    are notified.  The following information gets passed to each listener:
    
        struct os_time_change_info {
            /** UTC time prior to change. */
            const struct os_timeval *tci_prev_tv;
            /** Time zone prior to change. */
            const struct os_timezone *tci_prev_tz;
            /** UTC time after change. */
            const struct os_timeval *tci_cur_tv;
            /** Time zone after change. */
            const struct os_timezone *tci_cur_tz;
            /** True if the time was not set prior to change. */
            bool tci_newly_synced;
        };
    
    plus an optional listener-specific `void *arg`.

--- kernel/os/test/src/time_test.c
+++ kernel/os/test/src/time_test.c
@@ -6,7 +6,7 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
@@ -17,26 +17,11 @@
  * under the License.
  */
 
-#ifndef H_OS_TEST_
-#define H_OS_TEST_
+#include <string.h>
+#include "os/mynewt.h"
+#include "os_test_priv.h"
 
-#include "testutil/testutil.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-TEST_SUITE_DECL(os_mutex_test_suite);
-TEST_SUITE_DECL(os_sem_test_suite);
-TEST_SUITE_DECL(os_mempool_test_suite);
-
-TEST_SUITE_DECL(os_time_test_suite);
-TEST_CASE_DECL(os_time_test_change);
-
-int os_test_all(void);
-
-#ifdef __cplusplus
+TEST_SUITE(os_time_test_suite)
+{
+    os_time_test_change();
 }
-#endif
-
-#endif
