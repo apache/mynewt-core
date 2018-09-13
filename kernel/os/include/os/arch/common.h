3d9678de002484eb8a81be521cdff0cd37356217
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-09-13 23:32:12 +0200

    kernel/os: Remove obsolete API
    This is not used as public API anymore.

--- kernel/os/include/os/arch/common.h
+++ kernel/os/include/os/arch/common.h
@@ -59,7 +59,6 @@ struct os_task;
 #endif
 
 os_stack_t *os_arch_task_stack_init(struct os_task *, os_stack_t *, int);
-void timer_handler(void);
 void os_arch_ctx_sw(struct os_task *);
 os_sr_t os_arch_save_sr(void);
 void os_arch_restore_sr(os_sr_t);
