d915cea55165752a5ce8b329bfce79cf119eb5cc
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-09-13 22:10:11 +0200

    kernel/os: Remove obsolote os_bsp APIs
    They were moved to MCU long time ago...

--- kernel/os/include/os/arch/common.h
+++ kernel/os/include/os/arch/common.h
@@ -72,11 +72,6 @@ void os_set_env(os_stack_t *);
 void os_arch_init_task_stack(os_stack_t *sf);
 void os_default_irq_asm(void);
 
-/* External function prototypes supplied by BSP */
-void os_bsp_systick_init(uint32_t os_tick_per_sec, int prio);
-void os_bsp_idle(os_time_t ticks);
-void os_bsp_ctx_sw(void);
-
 #ifdef __cplusplus
 }
 #endif
