326e2e15598f8a56b8f04a534a1c2bdc8aa291f0
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2018-10-04 10:05:47 +0200

    spiflash: Add support for multiple chips
    SpiFlash chips are often compatible to some extent even
    when they come from different vendors.
    It is quite possible to have same product that will use
    different spiflash even in production.
    Now it is possible to specify more than one flash chip.

--- hw/drivers/flash/spiflash/include/spiflash/spiflash.h
+++ hw/drivers/flash/spiflash/include/spiflash/spiflash.h
@@ -35,6 +35,10 @@ struct spiflash_dev {
     int ss_pin;
     uint16_t sector_size;
     uint16_t page_size;
+    /* Array of supported flash chips */
+    const struct spiflash_chip *supported_chips;
+    /* Pointer to one of the supported chips */
+    const struct spiflash_chip *flash_chip;
 };
 
 extern struct spiflash_dev spiflash_dev;
@@ -46,12 +50,43 @@ extern struct spiflash_dev spiflash_dev;
 #define SPIFLASH_WRITE_ENABLE               0x06
 #define SPIFLASH_FAST_READ                  0x0B
 #define SPIFLASH_SECTOR_ERASE               0x20
+#define SPIFLASH_DEEP_POWER_DOWN            0xB9
 #define SPIFLASH_RELEASE_POWER_DOWN         0xAB
 #define SPIFLASH_READ_MANUFACTURER_ID       0x90
 #define SPIFLASH_READ_JEDEC_ID              0x9F
 
 #define SPIFLASH_STATUS_BUSY                0x01
 #define SPIFLASH_STATUS_WRITE_ENABLE        0x02
+
+/*
+ * Flash identification bytes from 0x9F command
+ */
+struct jedec_id {
+    uint8_t ji_manufacturer;
+    uint8_t ji_type;
+    uint8_t ji_capacity;
+};
+struct spiflash_chip {
+    struct jedec_id fc_jedec_id;
+    void (*fc_release_power_down)(struct spiflash_dev *dev);
+};
+
+#define JEDEC_MFC_ISSI              0x9D
+#define JEDEC_MFC_WINBOND           0xEF
+#define JEDEC_MFC_GIGADEVICE        0xC8
+#define JEDEC_MFC_MACRONIX          0xC2
+#define JEDEC_MFC_MICRON            0x20
+#define JEDEC_MFC_MICROCHIP         0xBF
+#define JEDEC_MFC_ADESTO            0x1F
+
+#define FLASH_CAPACITY_256KBIT      0x09
+#define FLASH_CAPACITY_512KBIT      0x10
+#define FLASH_CAPACITY_1MBIT        0x11
+#define FLASH_CAPACITY_2MBIT        0x12
+#define FLASH_CAPACITY_4MBIT        0x13
+#define FLASH_CAPACITY_8MBIT        0x14
+#define FLASH_CAPACITY_16MBIT       0x15
+#define FLASH_CAPACITY_32MBIT       0x16
 
 int spiflash_init(const struct hal_flash *dev);
 
