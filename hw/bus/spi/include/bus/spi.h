b1366267ddc917c26b3f763ea386adf44425a9f8
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-11-08 16:01:50 +0100

    hw/bus: Add SPI bus driver

---
+++ hw/bus/spi/include/bus/spi.h
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef HW_BUS_SPI_H_
+#define HW_BUS_SPI_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include "bus/bus.h"
+#include "bus/bus_driver.h"
+#include "bus/bus_debug.h"
+#include "hal/hal_spi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct bus_spi_dev_cfg {
+    int spi_num;
+    int pin_sck;
+    int pin_mosi;
+    int pin_miso;
+};
+
+struct bus_spi_dev {
+    struct bus_dev bdev;
+    struct bus_spi_dev_cfg cfg;
+
+#if MYNEWT_VAL(BUS_DEBUG)
+    uint32_t devmagic;
+#endif
+};
+
+#define BUS_SPI_MODE_0              (HAL_SPI_MODE0)
+#define BUS_SPI_MODE_1              (HAL_SPI_MODE1)
+#define BUS_SPI_MODE_2              (HAL_SPI_MODE2)
+#define BUS_SPI_MODE_3              (HAL_SPI_MODE3)
+
+#define BUS_SPI_DATA_ORDER_LSB      (HAL_SPI_LSB_FIRST)
+#define BUS_SPI_DATA_ORDER_MSB      (HAL_SPI_MSB_FIRST)
+
+struct bus_spi_node_cfg {
+    /** General node configuration */
+    struct bus_node_cfg node_cfg;
+    /** */
+    uint8_t pin_cs;
+    /** Data mode */
+    uint8_t mode;
+    /** Data order */
+    uint8_t data_order;
+    /** SCK frequency to be used for node */
+    uint16_t freq;
+    /** Quirks to be applied for device */
+    uint16_t quirks;
+};
+
+struct bus_spi_node {
+    struct bus_node bnode;
+    uint8_t pin_cs;
+    uint8_t mode;
+    uint8_t data_order;
+    uint16_t freq;
+    uint16_t quirks;
+
+#if MYNEWT_VAL(BUS_DEBUG)
+    uint32_t nodemagic;
+#endif
+};
+
+int
+bus_spi_dev_init_func(struct os_dev *odev, void *arg);
+
+static inline int
+bus_spi_dev_create(const char *name, struct bus_spi_dev *dev,
+                   struct bus_spi_dev_cfg *cfg)
+{
+    struct os_dev *odev = (struct os_dev *)dev;
+
+    return os_dev_create(odev, name, OS_DEV_INIT_PRIMARY, 0,
+                         bus_spi_dev_init_func, cfg);
+}
+
+int
+bus_spi_node_init_func(struct os_dev *odev, void *arg);
+
+static inline int
+bus_spi_node_create(const char *name, struct bus_spi_node *node,
+                    const struct bus_spi_node_cfg *cfg)
+{
+    struct os_dev *odev = (struct os_dev *)node;
+
+    return os_dev_create(odev, name, OS_DEV_INIT_PRIMARY, 1,
+                         bus_spi_node_init_func, (void *)cfg);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HW_BUS_SPI_H_ */
