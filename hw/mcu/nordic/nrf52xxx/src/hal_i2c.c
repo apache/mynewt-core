4afb948a3dba85ae0d924d62b42c9cadc91711a4
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2018-11-23 17:43:59 +0100

    hw/mcu/nordic: Update support for 380KHz I2C

--- hw/mcu/nordic/nrf52xxx/src/hal_i2c.c
+++ hw/mcu/nordic/nrf52xxx/src/hal_i2c.c
@@ -398,6 +398,9 @@ hal_i2c_config(uint8_t i2c_num, const struct hal_i2c_settings *cfg)
     case 250:
         freq = TWI_FREQUENCY_FREQUENCY_K250;
         break;
+    case 380:
+        freq = TWI_FREQUENCY_FREQUENCY_K380;
+        break;
     case 400:
         freq = TWI_FREQUENCY_FREQUENCY_K400;
         break;
