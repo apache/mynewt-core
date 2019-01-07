a27ecd149d033186ef04f7868bdb695dc8de2df4
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2019-01-07 11:16:14 +0100

    hw/util/i2cn: Fix parameter type
    OS ticks should be always os_time_t to avoid confusion.

--- hw/util/i2cn/src/i2cn.c
+++ hw/util/i2cn/src/i2cn.c
@@ -22,7 +22,7 @@
 
 int
 i2cn_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
-                 uint32_t timeout, uint8_t last_op, int retries)
+                 os_time_t timeout, uint8_t last_op, int retries)
 {
     int rc = 0;
     int i;
@@ -44,7 +44,7 @@ i2cn_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
 
 int
 i2cn_master_write(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
-                  uint32_t timeout, uint8_t last_op, int retries)
+                  os_time_t timeout, uint8_t last_op, int retries)
 {
     int rc = 0;
     int i;
