8a3ac1c306ec1608e1b05ec7c4aafe30ab562a0d
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2019-01-07 11:14:53 +0100

    hw/util/i2cn: Fix functions descriptions

--- hw/util/i2cn/include/i2cn/i2cn.h
+++ hw/util/i2cn/include/i2cn/i2cn.h
@@ -35,8 +35,8 @@ extern "C" {
  * @param i2c_num               The index of the I2C interface to read from.
  * @param pdata                 Additional parameters describing the read
  *                                  operation.
- * @param timeout               The time, in OS ticks, to wait for the MCU to
- *                                  indicate completion of each clocked byte.
+ * @param timeout               The time, in OS ticks, to wait for operation
+ *                                  completion.
  * @param last_op               1 if this is the final message in the
  *                                  transaction.
  *
@@ -53,8 +53,8 @@ int i2cn_master_read(uint8_t i2c_num, struct hal_i2c_master_data *pdata,
  * @param i2c_num               The index of the I2C interface to write to.
  * @param pdata                 Additional parameters describing the write
  *                                  operation.
- * @param timeout               The time, in OS ticks, to wait for the MCU to
- *                                  indicate completion of each clocked byte.
+ * @param timeout               The time, in OS ticks, to wait for operation
+ *                                  completion.
  * @param last_op               1 if this is the final message in the
  *                                  transaction.
  *
