fcefee6a0d61b1ed8ba3774f1de88844edb279bb
Fabio Utzig <utzig@apache.org>
2019-02-11 10:50:33 -0200

    [CRYPTO] Add STM32 driver

--- hw/mcu/stm/stm32f7xx/include/mcu/stm32_hal.h
+++ hw/mcu/stm/stm32f7xx/include/mcu/stm32_hal.h
@@ -77,8 +77,12 @@ struct stm32_hal_spi_cfg {
 
 #define STM32_HAL_TIMER_MAX     (3)
 
-/* hal_trng */
+/* hw/drivers/trng */
 #include "stm32f7xx_hal_rng.h"
+
+/* hw/drivers/crypto */
+#include "stm32f7xx_hal_cryp.h"
+#include "stm32f7xx_hal_rcc_ex.h"
 
 /* hal_flash */
 #include "stm32f7xx_hal_def.h"
