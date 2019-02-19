8ff9f4083f64548a3a6ab10b63c4d62277804b5b
Marko Kiiskila <marko@apache.org>
2019-02-19 14:00:36 +0200

    dialog_da1469x-dk-pro; Download script using flash_loader.

--- hw/bsp/dialog_da1469x-dk-pro/dialog_da1469x-dk-pro_download.sh
+++ hw/bsp/dialog_da1469x-dk-pro/dialog_da1469x-dk-pro_download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -29,31 +29,108 @@
 #  - FLASH_OFFSET contains the flash offset to download to
 #  - BOOT_LOADER is set if downloading a bootloader
 
-if [ -z "$CLI_PROGRAMMER" ]; then
-    CLI_PROGRAMMER=cli_programmer
-fi
+. $CORE_PATH/hw/scripts/jlink.sh
 
-if [ -z "$MKIMAGE" ]; then
-    MKIMAGE=mkimage
-fi
-
-. ${CORE_PATH}/hw/scripts/common.sh
 common_file_to_load
 
-JLinkGDBServer -device cortex-m33 -if swd -speed 4000 -if swd -port 3333 -singlerun &
+if [ "$MFG_IMAGE" ]; then
+    echo "Manufacturing image not supported yet"
+    exit 1
+fi
+
+parse_extra_jtag_cmd $EXTRA_JTAG_CMD
+
+GDB_CMD_FILE=.gdb_load
+JLINK_LOG_FILE=.jlink_log
+
+# flash_loader build for this BSP
+if [ -z $FLASH_LOADER ]; then
+    FL_TGT=da1469x_flash_loader
+    FLASH_LOADER=$BIN_ROOT/targets/$FL_TGT/app/apps/flash_loader/flash_loader.elf
+fi
+if [ ! -f $FLASH_LOADER ]; then
+    FILE=${FLASH_LOADER##$(pwd)/}
+    echo "Can't find flash_loader : $FILE"
+    if [ ! -z $FL_TGT ]; then
+	echo "Build $FL_TGT first"
+    fi
+    exit 1
+fi
+
+if [ ! -f $FILE_NAME ]; then
+    FILE=${FILE_NAME##$(pwd)/}
+    echo "Cannot find file" $FILE
+    exit 1
+fi
+
+hex2le() {
+    echo ${1:6:2}${1:4:2}${1:2:2}${1:0:2}
+}
+
+# XXX Write windows version also
+file_size() {
+    echo `wc -c $FILE_NAME | awk '{print $1}'`
+}
 
 if [ "$BOOT_LOADER" ]; then
-    VER_FILE=`mktemp`
-    IMG_FILE=`mktemp`
+    IMAGE_FILE=$FILE_NAME.img
 
-    trap "rm ${VER_FILE} ${IMG_FILE}" EXIT
+    # allocate 9K for header
+    dd if=/dev/zero bs=9k count=1 | LANG=C tr "\000" "\377" > ${IMAGE_FILE}
 
-    DATE=$(date -r ${FILE_NAME} +"%Y-%m-%d %H:%M")
-    echo "#define BLACKORCA_SW_VERSION \"v_1.0.0.0\"" >> ${VER_FILE}
-    echo "#define BLACKORCA_SW_VERSION_DATE \"${DATE}\"" >> ${VER_FILE}
-    ${MKIMAGE} d2522 ${FILE_NAME} ${VER_FILE} ${IMG_FILE}
+    # update product header
+    HEX="5070 00200000 00200000 eb00a5a8 66000000 aa11 0300 014007c84e"
+    echo -n ${HEX} | xxd -r -p | dd bs=1 of=${IMAGE_FILE} conv=notrunc
 
-    ${CLI_PROGRAMMER} -p 3333 gdbserver write_qspi 0x2000 ${IMG_FILE}
-else
-    ${CLI_PROGRAMMER} -p 3333 gdbserver write_qspi ${FLASH_OFFSET} ${FILE_NAME}
+    # update image header
+    SIZE=$(hex2le $(printf "%08x" $(file_size ${FILE_NAME})))
+    CRC32=$(hex2le $(crc32 ${FILE_NAME}))
+    HEX="5171 ${SIZE} ${CRC32} 00000000000000000000000000000000 00000000 00040000 aa22 0000 aa44 0000"
+    echo -n ${HEX} | xxd -r -p | dd bs=1 of=${IMAGE_FILE} seek=8k conv=notrunc
+
+    # append original image to header
+    cat $FILE_NAME >> ${IMAGE_FILE}
+
+    # use new image for flashing
+    FILE_NAME=${IMAGE_FILE}
 fi
+
+FILE_SIZE=$(file_size $FILE_NAME)
+
+cat > $GDB_CMD_FILE <<EOF
+set pagination off
+shell sh -c "trap '' 2; $JLINK_GDB_SERVER -device cortex-m33 -speed 4000 -if SWD -port $PORT -singlerun $EXTRA_JTAG_CMD > $JLINK_LOG_FILE 2>&1 &"
+target remote localhost:$PORT
+mon reset
+mon halt
+restore $FLASH_LOADER.bin binary 0x20000000
+symbol-file $FLASH_LOADER
+set \$sp=*(int *)0x20000000
+set \$pc=*(int *)0x20000004
+b main
+c
+d 1
+
+source $CORE_PATH/compiler/gdbmacros/flash_loader.gdb
+mon go
+
+# Wait for flash_loader to get ready
+printf "Waiting for flash loader to get ready...\n"
+while fl_state != 1
+end
+
+fl_erase 0 $FLASH_OFFSET $FILE_SIZE
+fl_program $FILE_NAME 0 $FLASH_OFFSET
+
+mon reset
+quit
+
+EOF
+
+arm-none-eabi-gdb -x $GDB_CMD_FILE
+
+# gdb appears to exit before jlinkgdbserver, so immediate
+# execution of debug script (i.e. from 'newt run'), will fail due
+# to 2 instances of jlinkgdbserver running. Slow exit from here for a bit.
+#
+sleep 1
