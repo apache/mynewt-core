d90e364de2196457c1e8b186b9a9d032f6bb4442
Christopher Collins <ccollins@apache.org>
2019-02-21 16:20:20 -0800

    mempool_tests: Fix stack corruption bug
    The "extended mempool" tests were causing stack corruption.  These tests
    registered mempools that were allocated on the stack and never
    unregistered them.  So, after these tests ran, the global mempool list
    contained some invalid entries.  Subsequently attempting to iterate or
    append to the list resulted in a crash or other unpredictable behavior.
    
    The solution is:
    1. Statically allocate the mempools.
    2. Always unregister the relevant mempool at the start of each test.
    
    This is now the non-extended tests are already implemented.

--- kernel/os/test/src/testcases/os_mempool_test_case.c
+++ kernel/os/test/src/testcases/os_mempool_test_case.c
@@ -36,6 +36,7 @@ mempool_test(int num_blocks, int block_size, bool clear)
     void *block;
     os_error_t rc;
 
+    /* Attempt to unregister the pool in case this test has already run. */
     os_mempool_unregister(&g_TstMempool);
 
     /* Check for too many blocks */
