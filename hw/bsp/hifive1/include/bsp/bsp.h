265b3171f75c1d0eddefbe0d5a78f81b7bf778be
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2019-03-04 13:55:48 +0100

    Add Arduino pin names to BSP's
    A lot of development boards have Arduino-like connectors.
    This adds common pin names to BSP so testing shields with
    different boards should be easier.

--- hw/bsp/hifive1/include/bsp/bsp.h
+++ hw/bsp/hifive1/include/bsp/bsp.h
@@ -69,6 +69,38 @@ extern "C" {
 #define HIFIVE_SPI1_SS2             (HIFIVE_PIN_15)
 #define HIFIVE_SPI1_SS3             (HIFIVE_PIN_16)
 
+/* Arduino pins */
+#define ARDUINO_PIN_D0              HIFIVE_PIN_0
+#define ARDUINO_PIN_D1              HIFIVE_PIN_1
+#define ARDUINO_PIN_D2              HIFIVE_PIN_2
+#define ARDUINO_PIN_D3              HIFIVE_PIN_3
+#define ARDUINO_PIN_D4              HIFIVE_PIN_4
+#define ARDUINO_PIN_D5              HIFIVE_PIN_5
+#define ARDUINO_PIN_D6              HIFIVE_PIN_6
+#define ARDUINO_PIN_D7              HIFIVE_PIN_7
+#define ARDUINO_PIN_D8              HIFIVE_PIN_8
+#define ARDUINO_PIN_D9              HIFIVE_PIN_9
+#define ARDUINO_PIN_D10             HIFIVE_PIN_10
+#define ARDUINO_PIN_D11             HIFIVE_PIN_11
+#define ARDUINO_PIN_D12             HIFIVE_PIN_12
+#define ARDUINO_PIN_D13             HIFIVE_PIN_13
+/* A0 pin is not connected  */
+#define ARDUINO_PIN_A1              HIFIVE_PIN_15
+#define ARDUINO_PIN_A2              HIFIVE_PIN_16
+#define ARDUINO_PIN_A3              HIFIVE_PIN_17
+#define ARDUINO_PIN_A4              HIFIVE_PIN_18
+#define ARDUINO_PIN_A5              HIFIVE_PIN_19
+
+#define ARDUINO_PIN_RX              ARDUINO_PIN_D0
+#define ARDUINO_PIN_TX              ARDUINO_PIN_D1
+
+#define ARDUINO_PIN_SCL             HIVIE_PIN_19
+#define ARDUINO_PIN_SDA             HIVIE_PIN_18
+
+#define ARDUINO_PIN_SCK             ARDUINO_PIN_D13
+#define ARDUINO_PIN_MOSI            ARDUINO_PIN_D11
+#define ARDUINO_PIN_MISO            ARDUINO_PIN_D12
+
 /* LED pins */
 #define HIFIVE_GREEN_LED_PIN        (HIFIVE_PIN_3)
 #define HIFIVE_BLUE_LED_PIN         (HIFIVE_PIN_5)
