7c51d32f5e9af3882169c31237f6eaf22b97b6ab
Łukasz Rymanowski <lukasz.rymanowski@codecoup.pl>
2019-03-15 14:37:57 +0100

    hw/bus: Allow to use nonblock spi operations
    In order to enable SPI_HAL_USE_NOBLOCK shall be set

--- hw/mcu/nordic/nrf52xxx/src/nrf52_periph.c
+++ hw/mcu/nordic/nrf52xxx/src/nrf52_periph.c
@@ -147,7 +147,7 @@ static const struct bus_spi_dev_cfg spi0_cfg = {
     .pin_mosi = MYNEWT_VAL(SPI_0_MASTER_PIN_MOSI),
     .pin_miso = MYNEWT_VAL(SPI_0_MASTER_PIN_MISO),
 };
-static struct bus_spi_dev spi0_bus;
+static struct bus_spi_hal_dev spi0_bus;
 #else
 static const struct nrf52_hal_spi_cfg os_bsp_spi0m_cfg = {
     .sck_pin      = MYNEWT_VAL(SPI_0_MASTER_PIN_SCK),
@@ -173,7 +173,7 @@ static const struct bus_spi_dev_cfg spi1_cfg = {
     .pin_mosi = MYNEWT_VAL(SPI_1_MASTER_PIN_MOSI),
     .pin_miso = MYNEWT_VAL(SPI_1_MASTER_PIN_MISO),
 };
-static struct bus_spi_dev spi1_bus;
+static struct bus_spi_hal_dev spi1_bus;
 #else
 static const struct nrf52_hal_spi_cfg os_bsp_spi1m_cfg = {
     .sck_pin      = MYNEWT_VAL(SPI_1_MASTER_PIN_SCK),
