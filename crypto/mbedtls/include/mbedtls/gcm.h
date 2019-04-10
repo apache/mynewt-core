14080ebd1b0810d11abb7e5d8ea2b11b04757854
Marko Kiiskila <marko@apache.org>
2019-04-10 09:29:38 +0300

    mbedtls; add a routine to add AAD data incrementally.

--- crypto/mbedtls/include/mbedtls/gcm.h
+++ crypto/mbedtls/include/mbedtls/gcm.h
@@ -235,6 +235,30 @@ int mbedtls_gcm_starts( mbedtls_gcm_context *ctx,
 
 /**
  * \brief           This function feeds an input buffer into an ongoing GCM
+ *                  encryption or decryption operation as additional data.
+ *                  This needs to be called before starting enc/dec
+ *                  operations.
+ *
+ *    `             The function expects input to be a multiple of 16
+ *                  Bytes. Only the last call before mbedtls_gcm_update() or
+ *                  mbedtls_gcm_finish() can be less than 16 Bytes.
+ *
+ *
+ * \param ctx       The GCM context.
+ * \param length    The length of the input data. This must be a multiple of
+ *                  16 except in the last call before mbedtls_gcm_finish().
+ * \param input     The buffer holding the input ADD.
+ *
+ * \return         \c 0 on success.
+ * \return         #MBEDTLS_ERR_GCM_BAD_INPUT on failure.
+ */
+int mbedtls_gcm_update_add( mbedtls_gcm_context *ctx,
+                size_t length,
+                const unsigned char *input );
+
+
+/**
+ * \brief           This function feeds an input buffer into an ongoing GCM
  *                  encryption or decryption operation.
  *
  *    `             The function expects input to be a multiple of 16
