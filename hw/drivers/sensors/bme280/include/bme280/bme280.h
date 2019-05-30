e22f59c4e8d782c94f401d21ae9c2cf3b6d8824d
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2019-05-30 16:05:50 +0200

    sensors/bme280: Update bus driver includes
    Some includes were not updated to correct values after
    bus driver changes.

--- hw/drivers/sensors/bme280/include/bme280/bme280.h
+++ hw/drivers/sensors/bme280/include/bme280/bme280.h
@@ -23,9 +23,8 @@
 
 #include "os/mynewt.h"
 #if MYNEWT_VAL(BUS_DRIVER_PRESENT)
-#include "bus/bus_driver.h"
-#include "bus/i2c.h"
-#include "bus/spi.h"
+#include "bus/drivers/i2c_common.h"
+#include "bus/drivers/spi_common.h"
 #endif
 #include "sensor/sensor.h"
 
