2bc43e0bba53f4d7be5b8225162da7217ecac367
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2019-06-03 10:34:05 +0200

    sensors: Enhance helper functions
    sensor_itf.si_dev has to have valid pointer to node.
    It could be done in sensor_itf variable declaration, but often
    this is the only filed that needs to be initialized and it is
    easy to overlook.
    
    Device creation helpers have both pointers in argument list
    so now they setup si_dev to correct value just to be sure.
    
    This change dose not affect existing code.

--- hw/drivers/sensors/lis2de12/src/lis2de12.c
+++ hw/drivers/sensors/lis2de12/src/lis2de12.c
@@ -3138,6 +3138,7 @@ lis2de12_create_i2c_sensor_dev(struct bus_i2c_node *node, const char *name,
 
     dev->node_is_spi = false;
 
+    sensor_itf->si_dev = &node->bnode.odev;
     bus_node_set_callbacks((struct os_dev *)node, &cbs);
 
     rc = bus_i2c_node_create(name, node, i2c_cfg, sensor_itf);
@@ -3158,6 +3159,7 @@ lis2de12_create_spi_sensor_dev(struct bus_spi_node *node, const char *name,
 
     dev->node_is_spi = true;
 
+    sensor_itf->si_dev = &node->bnode.odev;
     bus_node_set_callbacks((struct os_dev *)node, &cbs);
 
     rc = bus_spi_node_create(name, node, spi_cfg, sensor_itf);
