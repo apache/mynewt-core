2356adebd59d60bf56c1e10675e46087d22913d8
JustineKH <justine@juul.com>
2019-09-13 21:23:14 -0700

    mcu/dialog: define adc pin selections in mcu.h

--- hw/drivers/adc/sdadc_da1469x/src/sdadc_da1469x.c
+++ hw/drivers/adc/sdadc_da1469x/src/sdadc_da1469x.c
@@ -29,14 +29,14 @@
 static struct da1469x_sdadc_dev *da1469x_sdadc_dev;
 
 static const int da1469x_sdadc_src2pin[] = {
-    MCU_GPIO_PORT1(9),  /* 0 */
-    MCU_GPIO_PORT0(25), /* 1 */
-    MCU_GPIO_PORT0(8),  /* 2 */
-    MCU_GPIO_PORT0(9),  /* 3 */
-    MCU_GPIO_PORT1(14), /* 4 */
-    MCU_GPIO_PORT1(20), /* 5 */
-    MCU_GPIO_PORT1(21), /* 6 */
-    MCU_GPIO_PORT1(22), /* 7 */
+    MCU_SD_ADC_SRC0,  /* 0 */
+    MCU_SD_ADC_SRC1, /* 1 */
+    MCU_SD_ADC_SRC2,  /* 2 */
+    MCU_SD_ADC_SRC3,  /* 3 */
+    MCU_SD_ADC_SRC4, /* 4 */
+    MCU_SD_ADC_SRC5, /* 5 */
+    MCU_SD_ADC_SRC6, /* 6 */
+    MCU_SD_ADC_SRC7, /* 7 */
 };
 #define DA1469X_SDADC_SRC2PIN_SZ                                        \
     (sizeof(da1469x_sdadc_src2pin) / sizeof(da1469x_sdadc_src2pin[0]))
