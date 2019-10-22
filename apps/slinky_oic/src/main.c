e4b290d847f317c42a6228e82947395d237e8904
Manish <manish.varma@juul.com>
2019-10-22 10:51:18 -0700

    stats: Get rid of static qualifier for STATS_NAME_START

--- apps/slinky_oic/src/main.c
+++ apps/slinky_oic/src/main.c
@@ -29,7 +29,7 @@
 #include "split/split.h"
 #endif
 #include <bootutil/image.h>
-#include <imgmgr/imgmgr.h>
+#include <img_mgmt/img_mgmt.h>
 #include <mgmt/mgmt.h>
 #include <oic/oc_api.h>
 #include <assert.h>
@@ -68,7 +68,7 @@ STATS_SECT_END
 
 static STATS_SECT_DECL(gpio_stats) g_stats_gpio_toggle;
 
-static STATS_NAME_START(gpio_stats)
+STATS_NAME_START(gpio_stats)
 STATS_NAME(gpio_stats, toggles)
 STATS_NAME_END(gpio_stats)
 
@@ -148,7 +148,7 @@ task1_handler(void *arg)
     g_led_pin = LED_BLINK_PIN;
     hal_gpio_init_out(g_led_pin, 1);
 
-    if (imgr_my_version(&ver) == 0) {
+    if (img_mgmt_my_version(&ver) == 0) {
         console_printf("\nSlinky_OIC %u.%u.%u.%u\n",
           ver.iv_major, ver.iv_minor, ver.iv_revision,
           (unsigned int)ver.iv_build_num);
