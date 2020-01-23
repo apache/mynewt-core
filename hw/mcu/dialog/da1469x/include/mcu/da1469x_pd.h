18c99e4fc1f51fd6f66b72ef2abeb27ec177ab0d
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2020-01-23 12:05:53 +0100

    hw/mcu/dialog: Remove PD_RAD from PD control
    We don't need/want to control PD_RAD domain explicitly, it's just always
    allowed to sleep so will be automatically controlled by CMAC.

--- hw/mcu/dialog/da1469x/include/mcu/da1469x_pd.h
+++ hw/mcu/dialog/da1469x/include/mcu/da1469x_pd.h
@@ -31,11 +31,10 @@ extern "C" {
 /* Available (controllable) power domains */
 #define MCU_PD_DOMAIN_SYS           0
 #define MCU_PD_DOMAIN_PER           1
-#define MCU_PD_DOMAIN_RAD           2
-#define MCU_PD_DOMAIN_TIM           3
-#define MCU_PD_DOMAIN_COM           4
+#define MCU_PD_DOMAIN_TIM           2
+#define MCU_PD_DOMAIN_COM           3
 
-#define MCU_PD_DOMAIN_COUNT         5
+#define MCU_PD_DOMAIN_COUNT         4
 
 int da1469x_pd_init(void);
 int da1469x_pd_acquire(uint8_t pd);
