51401a1b58c07120924f929c1769c0cea0b11943
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2021-01-08 12:38:18 +0100

    hw/mcu/da1469x: Use spi_da1469x driver instead of spi_hal
    DA1469x now has dedicated SPI bus driver that will be
    used instead of generic spi_hal.

--- hw/mcu/dialog/da1469x/src/da1469x_periph.c
+++ hw/mcu/dialog/da1469x/src/da1469x_periph.c
@@ -67,7 +67,7 @@ static struct crypto_dev os_bsp_crypto;
 
 #if MYNEWT_VAL(SPI_0_MASTER) || MYNEWT_VAL(SPI_1_MASTER)
 #if MYNEWT_VAL(BUS_DRIVER_PRESENT)
-#include "bus/drivers/spi_hal.h"
+#include "bus/drivers/spi_da1469x.h"
 #else
 #include "hal/hal_spi.h"
 #endif
@@ -175,7 +175,7 @@ static const struct bus_spi_dev_cfg spi0_cfg = {
     .pin_mosi = MYNEWT_VAL(SPI_0_MASTER_PIN_MOSI),
     .pin_miso = MYNEWT_VAL(SPI_0_MASTER_PIN_MISO),
 };
-static struct bus_spi_hal_dev spi0_bus;
+static struct bus_spi_dev spi0_bus;
 #else
 static const struct da1469x_hal_spi_cfg hal_spi0_cfg = {
     .pin_sck = MYNEWT_VAL(SPI_0_MASTER_PIN_SCK),
@@ -199,7 +199,7 @@ static const struct bus_spi_dev_cfg spi1_cfg = {
     .pin_mosi = MYNEWT_VAL(SPI_1_MASTER_PIN_MOSI),
     .pin_miso = MYNEWT_VAL(SPI_1_MASTER_PIN_MISO),
 };
-static struct bus_spi_hal_dev spi1_bus;
+static struct bus_spi_dev spi1_bus;
 #else
 static const struct da1469x_hal_spi_cfg hal_spi1_cfg = {
     .pin_sck = MYNEWT_VAL(SPI_1_MASTER_PIN_SCK),
@@ -410,8 +410,8 @@ da1469x_periph_create_spi(void)
 
 #if MYNEWT_VAL(SPI_0_MASTER)
 #if MYNEWT_VAL(BUS_DRIVER_PRESENT)
-    rc = bus_spi_hal_dev_create("spi0", &spi0_bus,
-                                (struct bus_spi_dev_cfg *)&spi0_cfg);
+    rc = bus_spi_da1469x_dev_create("spi0", &spi0_bus,
+                                    (struct bus_spi_dev_cfg *)&spi0_cfg);
     assert(rc == 0);
 #else
     rc = hal_spi_init(0, (void *)&hal_spi0_cfg, HAL_SPI_TYPE_MASTER);
@@ -421,8 +421,8 @@ da1469x_periph_create_spi(void)
 
 #if MYNEWT_VAL(SPI_1_MASTER)
 #if MYNEWT_VAL(BUS_DRIVER_PRESENT)
-    rc = bus_spi_hal_dev_create("spi1", &spi1_bus,
-                                (struct bus_spi_dev_cfg *)&spi1_cfg);
+    rc = bus_spi_da1469x_dev_create("spi1", &spi1_bus,
+                                    (struct bus_spi_dev_cfg *)&spi1_cfg);
     assert(rc == 0);
 #else
     rc = hal_spi_init(1, (void *)&hal_spi1_cfg, HAL_SPI_TYPE_MASTER);
