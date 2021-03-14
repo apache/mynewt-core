1f7582504bbceba180d27df432260523f343d05f
Ildar Mulyukov <ildar@altlinux.ru>
2021-03-14 19:08:25 +0600

    hw/drivers/flash/spiflash: add JEDEC_MFC_XTX and XT25F32B chip
    for PineTime smart watch

--- hw/drivers/flash/spiflash/include/spiflash/spiflash.h
+++ hw/drivers/flash/spiflash/include/spiflash/spiflash.h
@@ -126,6 +126,7 @@ struct spiflash_chip {
 #define JEDEC_MFC_MICROCHIP         0xBF
 #define JEDEC_MFC_ADESTO            0x1F
 #define JEDEC_MFC_EON               0x1C
+#define JEDEC_MFC_XTX               0x0B
 
 #define FLASH_CAPACITY_256KBIT      0x09
 #define FLASH_CAPACITY_512KBIT      0x10
