7d77ac7738908d0a9ad5d2495f12cda41cde59bc
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2022-03-31 14:13:05 +0200

    mcu/nordic: Build fixes after nrfx 2.7 upgrade
    Cosmetic changes due to NRFX 2.7 header and source upgrade

--- hw/mcu/nordic/nrf5340/src/system_nrf5340.c
+++ hw/mcu/nordic/nrf5340/src/system_nrf5340.c
@@ -97,7 +97,7 @@ void SystemInit(void)
                 /* IAR will complain about the order of volatile pointer accesses. */
                 #pragma diag_suppress=Pa082
             #endif
-            *NRF_FICR_S->TRIMCNF[index].ADDR = NRF_FICR_S->TRIMCNF[index].DATA;
+            *((volatile uint32_t *)NRF_FICR_S->TRIMCNF[index].ADDR) = NRF_FICR_S->TRIMCNF[index].DATA;
             #if defined ( __ICCARM__ )
                 #pragma diag_default=Pa082
             #endif
