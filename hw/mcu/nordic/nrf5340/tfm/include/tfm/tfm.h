4a74799dfaed210cdd0f80c535e54e3851d7d743
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2022-10-11 09:27:23 +0200

    mcu/nrf5340: Add secure functions package
    New package will contain secure functions.
    Those functions will allow access to secure only peripherals.
    
    To start MCU assignment to GPIO function tfm_gpio_pin_mcu_select
    allows to redirect PIN to net core.
    
    tfm_uicr_otp_read() allows to read UICR->OTP from non-secure code.
    
    When tfm package build with bootloader or secure application
    functions are treated as every other function.
    
    When tfm is build with non-secure application function bodies
    are not present in the build and are expected to be linked with
    library that provides secure gateways.
    
    tfm can be also build into bootloader in this case it's the
    bootloader that provides secure functions.
    
    ELF that provides implementation of secure functions must be
    build with TFM_EXPORT_NSC: 1 syscfg value.
    
    Signed-off-by: Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>

---
+++ hw/mcu/nordic/nrf5340/tfm/include/tfm/tfm.h
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <stdint.h>
+#include <os/mynewt.h>
+#include <nrf_gpio.h>
+
+#if (MYNEWT_VAL(BOOT_LOADER) && MYNEWT_VAL(TFM_EXPORT_NSC)) || \
+    (!MYNEWT_VAL(BOOT_LOADER) && MYNEWT_VAL(MCU_APP_SECURE) && MYNEWT_VAL(TFM_EXPORT_NSC)) || \
+    (!MYNEWT_VAL(BOOT_LOADER) && !MYNEWT_VAL(MCU_APP_SECURE))
+#define SECURE_CALL __attribute__((cmse_nonsecure_entry))
+#else
+/* Whole application is secure */
+#define SECURE_CALL
+#endif
+
+#define TFM_ERR_ACCESS_DENIED   SYS_EACCES
+#define TFM_ERR_INVALID_PARAM   SYS_EINVAL
+
+/**
+ * Read UICR OTP word
+ *
+ * @param n   - word to read
+ * @param ret - buffer for received value
+ * @return 0 on success
+ *         TFM_ERR_INVALID_PARAM - when n is out of range <0-191>
+ *         TFM_ERR_ACCESS_DENIED - when n is denied by secure code
+ */
+int SECURE_CALL tfm_uicr_otp_read(uint8_t n, uint32_t *ret);
+
+/**
+ *
+ * @param pin_number
+ * @param mcu_sel
+ * @return 0 on success
+ *         TFM_ERR_INVALID_PARAM - when pin_number is not present on chip
+ *         TFM_ERR_ACCESS_DENIED - when non-secure code is not allowed to change pin's MCU
+ */
+int SECURE_CALL tfm_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_mcusel_t mcu_sel);
---
+++ hw/mcu/nordic/nrf5340/tfm/src/tfm.c
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <os/mynewt.h>
+#include <nrf_gpio.h>
+#include <tfm/tfm.h>
+
+int SECURE_CALL
+tfm_uicr_otp_read(uint8_t n, uint32_t *ret)
+{
+    int err = 0;
+
+    if (n >= 192) {
+        err = TFM_ERR_INVALID_PARAM;
+    } else if (n < MYNEWT_VAL(TFM_UICR_OTP_MIN_ADDR) || n > MYNEWT_VAL(TFM_UICR_OTP_MAX_ADDR)) {
+        err = TFM_ERR_ACCESS_DENIED;
+    } else {
+        *ret = NRF_UICR_S->OTP[n];
+    }
+
+    return err;
+}
+
+int SECURE_CALL
+tfm_gpio_pin_mcu_select(uint32_t pin_number, nrf_gpio_pin_mcusel_t mcu_sel)
+{
+    int err = 0;
+    uint32_t pin_mask = (1u << (pin_number & 31));
+
+    if (!nrf_gpio_pin_present_check(pin_number)) {
+        err = TFM_ERR_INVALID_PARAM;
+    } else if (((pin_number < 32) && (MYNEWT_VAL(TFM_MCU_SEL_GPIO0) & pin_mask)) ||
+               ((pin_number >= 32) && (pin_number < 64) &&
+                (MYNEWT_VAL(TFM_MCU_SEL_GPIO1) & pin_mask))) {
+        nrf_gpio_pin_mcu_select(pin_number, mcu_sel);
+    } else {
+        err = TFM_ERR_ACCESS_DENIED;
+    }
+
+    return err;
+}
