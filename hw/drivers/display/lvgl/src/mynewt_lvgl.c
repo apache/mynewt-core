3bea7ba722aeb3e22488a8297d59154c333dce32
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2023-09-11 09:29:52 +0200

    lvgl: Display driver optimizations
    This adds several syscfg values that tweak LVGL build.
    LV_DISP_DRAW_BUF_LINES (default 10) specifies buffer size in lines.
    Increasing this number can reduce drawing time but uses more memory.
    LV_DISP_DOUBLE_BUFFER - when set, allows display driver to draw
    pixel asynchronously (with DMA). Currently no driver utilizes this.
    LV_DISP_X_ALIGN/LV_DISP_Y_ALIGN - can specify minimum pixel alignment
    required by display driver. Currently only DA1469X LCD driver requires
    LV_DISP_X_ALIGN to be 1. Increasing this number forces LVGL to draw more
    pixels that library needs.
    
    Signed-off-by: Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>

--- hw/drivers/display/lvgl/src/mynewt_lvgl.c
+++ hw/drivers/display/lvgl/src/mynewt_lvgl.c
@@ -50,6 +50,7 @@ init_lv_timer(void)
     os_callout_reset(&lv_callout, lv_timer_period);
 }
 
+#define DISP_DRAW_BUF_LINES MYNEWT_VAL(LV_DISP_DRAW_BUF_LINES)
 /* A static or global variable to store the buffers */
 static lv_disp_draw_buf_t disp_buf;
 /* A variable to hold the drivers. Must be static or global. */
@@ -57,8 +58,36 @@ static lv_disp_drv_t disp_drv;
 #define DISP_HOR_RES MYNEWT_VAL(LVGL_DISPLAY_HORIZONTAL_RESOLUTION)
 #define DISP_VERT_RES MYNEWT_VAL(LVGL_DISPLAY_VERTICAL_RESOLUTION)
 /* Static or global buffer(s). The second buffer is optional */
-static lv_color_t buf_1[DISP_HOR_RES * 10];
-static lv_color_t buf_2[DISP_HOR_RES * 10];
+static lv_color_t buf_1[DISP_HOR_RES * DISP_DRAW_BUF_LINES];
+#if MYNEWT_VAL(LV_DISP_DOUBLE_BUFFER)
+static lv_color_t buf_2[DISP_HOR_RES * DISP_DRAW_BUF_LINES];
+#else
+#define buf_2 NULL
+#endif
+
+/** Extend the invalidated areas to match with the display drivers requirements
+ * E.g. round `y` to, 8, 16 ..) on a monochrome display*/
+static void
+mynewt_lv_rounder(struct _lv_disp_drv_t *driver, lv_area_t *area)
+{
+    (void)driver;
+    uint32_t mask;
+
+    if (MYNEWT_VAL(LV_DISP_X_ALIGN)) {
+        mask = (1 << MYNEWT_VAL(LV_DISP_X_ALIGN)) - 1;
+        /* Clear left side bits */
+        area->x1 &= ~mask;
+        /* Set right side bits */
+        area->x2 |= mask;
+    }
+    if (MYNEWT_VAL(LV_DISP_Y_ALIGN)) {
+        mask = (1 << MYNEWT_VAL(LV_DISP_Y_ALIGN)) - 1;
+        /* Clear top side bits */
+        area->y1 &= ~mask;
+        /* Set bottom side bits */
+        area->y2 |= mask;
+    }
+}
 
 void
 mynewt_lv_init(void)
@@ -70,11 +99,14 @@ mynewt_lv_init(void)
     lv_init();
 
     /*Initialize `disp_buf` with the buffer(s). With only one buffer use NULL instead buf_2 */
-    lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, DISP_HOR_RES * 10);
+    lv_disp_draw_buf_init(&disp_buf, buf_1, buf_2, DISP_HOR_RES * DISP_DRAW_BUF_LINES);
     lv_disp_drv_init(&disp_drv);            /* Basic initialization */
     disp_drv.draw_buf = &disp_buf;          /* Set an initialized buffer */
     disp_drv.hor_res = DISP_HOR_RES;        /* Set the horizontal resolution in pixels */
     disp_drv.ver_res = DISP_VERT_RES;       /* Set the vertical resolution in pixels */
+    if (MYNEWT_VAL(LV_DISP_X_ALIGN) || MYNEWT_VAL(LV_DISP_Y_ALIGN)) {
+        disp_drv.rounder_cb = mynewt_lv_rounder;
+    }
 
     mynewt_lv_drv_init(&disp_drv);
 
