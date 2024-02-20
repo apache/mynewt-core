8619cd7aa807c11568c7aac57e3785c9cf7002bb
Jerzy Kasenberg <jerzy.kasenberg@codecoup.pl>
2024-02-20 10:33:08 +0100

    util/crc: Add crc32 and Adler32 algorithms
    This adds two simple implementations of standard CRC32
    and Adler32 algorithms that can be used on block of data
    and in streams where result from previous call can
    be used as inptu to continue computation.
    
    No prepcomputed table are used for now.

---
+++ util/crc/include/crc/crc32.h
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+#ifndef _CRC32_H_
+#define _CRC32_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t crc32_init(void);
+uint32_t crc32_calc(uint32_t val, const void *buf, size_t cnt);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CRC16_H_ */
