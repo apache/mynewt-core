cd17a80c4a4884393b19b297e409185e7bf920f7
Magdalena Kasenberg <magdalena.kasenberg@codecoup.pl>
2024-11-08 11:48:47 +0100

    hw/mcu/nordic/nrf5340_net/src/hal_os_tick: Use cputimer instead of RTC1

--- hw/mcu/nordic/nrf5340_net/src/hal_os_tick.c
+++ hw/mcu/nordic/nrf5340_net/src/hal_os_tick.c
@@ -29,10 +29,6 @@
 #endif
 
 #define RTC_FREQ            32768       /* in Hz */
-#define OS_TICK_TIMER       NRF_RTC1_NS
-#define OS_TICK_IRQ         RTC1_IRQn
-#define OS_TICK_CMPREG      3
-#define RTC_COMPARE_INT_MASK(ccreg) (1UL << ((ccreg) + 16))
 
 struct hal_os_tick {
     int ticks_per_ostick;
@@ -41,6 +37,7 @@ struct hal_os_tick {
 };
 
 struct hal_os_tick g_hal_os_tick;
+struct hal_timer os_tick_timer;
 
 /*
  * Implement (x - y) where the range of both 'x' and 'y' is limited to 24-bits.
@@ -72,39 +69,19 @@ sub24(uint32_t x, uint32_t y)
 static inline uint32_t
 nrf5340_net_os_tick_counter(void)
 {
-    return OS_TICK_TIMER->COUNTER;
+    return os_cputime_get32();
 }
 
 static inline void
 nrf5340_net_os_tick_set_ocmp(uint32_t ocmp)
 {
-    int delta;
-    uint32_t counter;
-
     OS_ASSERT_CRITICAL();
-    while (1) {
-        ocmp &= 0xffffff;
-        OS_TICK_TIMER->CC[OS_TICK_CMPREG] = ocmp;
-        counter = nrf5340_net_os_tick_counter();
-        /*
-         * From nRF5340 Product specification
-         *
-         * - If Counter is 'N' writing (N) or (N + 1) to CC register
-         *   may not trigger a compare event.
-         *
-         * - If Counter is 'N' writing (N + 2) to CC register is guaranteed
-         *   to trigger a compare event at 'N + 2'.
-         */
-        delta = sub24(ocmp, counter);
-        if (delta > 2) {
-            break;
-        }
-        ocmp += g_hal_os_tick.ticks_per_ostick;
-    }
+    ocmp &= 0xffffff;
+    os_cputime_timer_start(&os_tick_timer, ocmp);
 }
 
 static void
-nrf5340_net_timer_handler(void)
+nrf5340_net_timer_handler(void *arg)
 {
     int delta;
     int ticks;
@@ -122,7 +99,6 @@ nrf5340_net_timer_handler(void)
     os_time_advance(ticks);
 
     /* Clear timer interrupt */
-    OS_TICK_TIMER->EVENTS_COMPARE[OS_TICK_CMPREG] = 0;
 
     /* Update the time associated with the most recent tick */
     g_hal_os_tick.lastocmp = (g_hal_os_tick.lastocmp +
@@ -161,15 +137,13 @@ os_tick_idle(os_time_t ticks)
          * Update OS time before anything else when coming out of
          * the tickless regime.
          */
-        nrf5340_net_timer_handler();
+        nrf5340_net_timer_handler(NULL);
     }
 }
 
 void
 os_tick_init(uint32_t os_ticks_per_sec, int prio)
 {
-    uint32_t sr;
-
     assert(RTC_FREQ % os_ticks_per_sec == 0);
 
     g_hal_os_tick.lastocmp = 0;
@@ -182,29 +156,6 @@ os_tick_init(uint32_t os_ticks_per_sec, int prio)
      */
     g_hal_os_tick.max_idle_ticks = (1UL << 22) / g_hal_os_tick.ticks_per_ostick;
 
-    /* disable interrupts */
-    OS_ENTER_CRITICAL(sr);
-
-    /* Set isr in vector table and enable interrupt */
-    NVIC_SetPriority(OS_TICK_IRQ, prio);
-    NVIC_SetVector(OS_TICK_IRQ, (uint32_t)nrf5340_net_timer_handler);
-    NVIC_EnableIRQ(OS_TICK_IRQ);
-
-    /*
-     * Program the OS_TICK_TIMER to operate at 32KHz and trigger an output
-     * compare interrupt at a rate of 'os_ticks_per_sec'.
-     */
-    OS_TICK_TIMER->TASKS_STOP = 1;
-    OS_TICK_TIMER->TASKS_CLEAR = 1;
-
-    OS_TICK_TIMER->EVTENCLR = 0xffffffff;
-    OS_TICK_TIMER->INTENCLR = 0xffffffff;
-    OS_TICK_TIMER->INTENSET = RTC_COMPARE_INT_MASK(OS_TICK_CMPREG);
-
-    OS_TICK_TIMER->EVENTS_COMPARE[OS_TICK_CMPREG] = 0;
-    OS_TICK_TIMER->CC[OS_TICK_CMPREG] = g_hal_os_tick.ticks_per_ostick;
-
-    OS_TICK_TIMER->TASKS_START = 1;
-
-    OS_EXIT_CRITICAL(sr);
+    os_cputime_timer_init(&os_tick_timer, nrf5340_net_timer_handler, NULL);
+    os_cputime_timer_start(&os_tick_timer, g_hal_os_tick.ticks_per_ostick);
 }
