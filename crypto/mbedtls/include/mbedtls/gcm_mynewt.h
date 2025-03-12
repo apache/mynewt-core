9023e86b5e423bbbf14ddc75a2d4668eb1c6764f
Michal Gorecki <michal.gorecki@codecoup.pl>
2025-03-12 12:27:36 +0100

    crypto/mbedtls: Upgrade to v3.6.5
    This upgrades mbedtls version to v3.6.5:
    1. mbedtls repository changed to version v3.6.5
    2. Removed tests that are no longer supported
    3. Updated/modified gcm_mynewt API:
       a) Function mbedtls_gcm_update_add is removed, because now
          function mbedtls_gcm_update_ad from mbedtls upstream
          provides the same functionality.
       b) Due to internal mbedtls API changes mbedtls_gcm_setkey_noalloc
          function now needs one argument more, which is keybits.
          This argument specifies length of key in bits and should
          be set to 128, 192 or 256. Other values won't be accepted.
          In earlier version this could be extracted from
          mbedtls_cipher_info_t key_bitlen field, which is no longer
          the case - now it's 4 bit bitfield later processed by
          internal mbedtls macros.
       It probably would be possible to not break this APIs, but
       since a lot of mbedtls APIs where changed in new version
       it was assumed that migration changes in projects using mbedtls
       would be necessary anyway.

--- crypto/mbedtls/include/mbedtls/gcm_mynewt.h
+++ crypto/mbedtls/include/mbedtls/gcm_mynewt.h
@@ -20,31 +20,8 @@
 #ifndef _GCM_MYNEWT_H_
 #define _GCM_MYNEWT_H_
 
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
 #include <mbedtls/gcm.h>
-
-/**
- * \brief           This function feeds an input buffer into an ongoing GCM
- *                  encryption or decryption operation as additional data.
- *                  This needs to be called before starting enc/dec
- *                  operations.
- *
- *    `             The function expects input to be a multiple of 16
- *                  Bytes. Only the last call before mbedtls_gcm_update() or
- *                  mbedtls_gcm_finish() can be less than 16 Bytes.
- *
- *
- * \param ctx       The GCM context.
- * \param length    The length of the input data. This must be a multiple of
- *                  16 except in the last call before mbedtls_gcm_finish().
- * \param input     The buffer holding the input ADD.
- *
- * \return         \c 0 on success.
- * \return         #MBEDTLS_ERR_GCM_BAD_INPUT on failure.
- */
-int mbedtls_gcm_update_add( mbedtls_gcm_context *ctx,
-                            size_t length,
-                            const unsigned char *input );
-
 
 /**
  * Same as mbedtls_gcm_setkey, but with preallocated memory for cipher algorithm context
@@ -52,6 +29,7 @@ int mbedtls_gcm_update_add( mbedtls_gcm_context *ctx,
 int mbedtls_gcm_setkey_noalloc( mbedtls_gcm_context *ctx,
                                 const mbedtls_cipher_info_t *cipher_info,
                                 const unsigned char *key,
+                                unsigned int keybits,
                                 void *cipher_ctx);
 
 
