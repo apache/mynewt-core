00fd862e268bd281e417ad81bd706c11faec5abb
Jerzy Kasenberg <jerzy@apache.org>
2025-05-31 00:40:59 +0200

    sys/shell: Add macros for creating link time commands
    Shell commands and modules used to be registered at
    run time by packages. This required code and RAM space.
    Both shell modules and commands required RAM of some fixed
    size (specified in syscfg). When more commands or shell
    modules were registered assertion failed at run time.
    
    Now it's possibly to create shell commands that will be
    'registered' at link time and will not require RAM
    nor explicit function call.
    This should eliminate problem of registering to
    many modules or commands.
    
    Signed-off-by: Jerzy Kasenberg <jerzy@apache.org>

--- sys/shell/include/shell/shell.h
+++ sys/shell/include/shell/shell.h
@@ -25,6 +25,7 @@ extern "C" {
 #endif
 
 #include "os/mynewt.h"
+#include "os/link_tables.h"
 #include "streamer/streamer.h"
 
 struct os_eventq;
@@ -84,6 +85,192 @@ struct shell_module {
     const char *name;
     const struct shell_cmd *commands;
 };
+
+typedef const struct shell_mod *shell_mod_t;
+typedef const struct shell_cmd *shell_cmd_t;
+
+/*
+ * Shell module enumeration functions
+ */
+typedef struct shell_mod_ops {
+    /** Returns number of module commands */
+    size_t (*count)(shell_mod_t mod);
+    /** Return module command by it's index */
+    shell_cmd_t (*get)(shell_mod_t mod, size_t ix);
+} shell_mod_ops_t;
+
+/*
+ * Shell module
+ */
+struct shell_mod {
+    /* Shell module functions */
+    const shell_mod_ops_t *ops;
+    /* Shell module name visible to end use */
+    const char *name;
+};
+
+/*
+ * Shell module with static array of commands
+ */
+typedef struct shell_mod_std {
+    /* Inherited shell module */
+    struct shell_mod mod;
+    /* Beginning of command array */
+    const struct shell_cmd *commands;
+    /* Pointer to end of command array */
+    const struct shell_cmd *commands_end;
+} shell_mod_std_t;
+extern const struct shell_mod_ops shell_mod_std_ops;
+
+static inline int
+shell_mod_command_count(shell_mod_t mod)
+{
+    return mod->ops->count(mod);
+}
+
+static inline shell_cmd_t
+shell_mod_command(shell_mod_t mod, size_t ix)
+{
+    return mod->ops->get(mod, ix);
+}
+
+LINK_TABLE(shell_mod_t, shell_modules)
+/* Section name for shell modules */
+#define SHELL_MODULES_SECTION LINK_TABLE_SECTION(shell_modules)
+#define SHELL_MODULE_SECTION(mod)                                             \
+    LINK_TABLE_ELEMENT_SECTION(shell_modules, mod)
+
+/**
+ * Create shell module with commands designated by begin and end pointers
+ *
+ * This will create object of type shell_mod_std and pointer to this object.
+ * Pointer will be placed in .shell_modules section so it will be put
+ * together with all other shell modules on one link time table.
+ *
+ * @param _name - name of the module
+ * @param _command_begin - pointer to start of the table with commands
+ * @param _command_end - pointer to end of the table with commands
+ */
+#define SHELL_MODULE_STD(_name, _command_begin, _command_end)                 \
+    const shell_mod_std_t shell_module_##_name = {                            \
+        { &shell_mod_std_ops, #_name },                                       \
+        _command_begin, _command_end                                          \
+    };                                                                        \
+    LINK_TABLE_ELEMENT_REF(shell_modules, _name, shell_module_##_name.mod)
+
+/**
+ * Create shell module with static command table
+ *
+ * @param _name - name of the module
+ * @parma _table - standard table of const struct shell_cmd with know size
+ */
+#define SHELL_MODULE_WITH_TABLE(name_, table_)                                \
+    SHELL_MODULE_STD(name_, table_, table_ + ARRAY_SIZE(table_))
+
+#define SHELL_MODULE_CMD_SECTION(mod_, cmd_)                                  \
+    LINK_TABLE_ELEMENT_SECTION(mod_##_commands, cmd_)
+/* Section name for shell compat (global) commands */
+#define SHELL_COMPAT_CMDS_SECTION LINK_TABLE_SECTION(compat_commands)
+/* Section name for shell compat command */
+#define SHELL_COMPAT_CMD_SECTION(cmd_) SHELL_MODULE_CMD_SECTION(compat, cmd_)
+
+/**
+ * Create shell module with link time table (name derived from _name)
+ *
+ * Link table will have name derived from shell module.
+ * Link table must be declared in some pkg.yml in pkg.link_tables: section
+ *
+ * i.e.
+ * pkg.yml
+ *
+ * pkg.link_tables:
+ *     - mcu_commands
+ *
+ * mcu_cli.c
+ *
+ * SHELL_MODULE_WITH_LINK_TABLE(mcu)
+ *
+ * @param _name - name of the shell module
+ */
+#define SHELL_MODULE_WITH_LINK_TABLE(_name)                                   \
+    LINK_TABLE(struct shell_cmd, _name##_commands)                            \
+    SHELL_MODULE_STD(_name, LINK_TABLE_START(_name##_commands),               \
+                     LINK_TABLE_END(_name##_commands))
+
+/**
+ * Create command for module
+ *
+ * Macro creates object of struct shell_cmd and puts it in link table
+ * for module
+ *
+ * @param mod_ - module name
+ * @param cmd_ - command name
+ * @param name_ - command name as string
+ * @param ext_ - 1 for extended command with streamer
+ * @param func_ - function to execute
+ * @param help_ - help structure for function
+ */
+#define SHELL_MODULE_CMD_WITH_NAME(mod_, cmd_, name_, ext_, func_, help_)            \
+    const struct shell_cmd shell_cmd_##cmd_ SHELL_MODULE_CMD_SECTION(mod_, cmd_) = { \
+        .sc_ext = ext_,                                                              \
+        .sc_cmd_ext_func = (shell_cmd_ext_func_t)(func_),                            \
+        .sc_cmd = name_,                                                             \
+        .help = SHELL_HELP_(help_),                                                  \
+    };
+
+/**
+ * Create command for module
+ *
+ * Macro creates object of struct shell_cmd and puts it in link table
+ * for module
+ *
+ * @param mod_ - module name
+ * @param cmd_ - command name
+ * @param func_ - function to execute
+ * @param help_ - help structure for function
+ */
+#define SHELL_MODULE_CMD(mod_, cmd_, func_, help_)                            \
+    SHELL_MODULE_CMD_WITH_NAME(mod_, cmd_, #cmd_, 0, func_, help_)
+
+/**
+ * Create command for module
+ *
+ * Macro creates object of struct shell_cmd and puts it in link table
+ * for module
+ *
+ * @param mod_ - module name
+ * @param cmd_ - command name
+ * @param func_ - function to execute
+ * @param help_ - help structure for function
+ */
+#define SHELL_MODULE_EXT_CMD(mod_, cmd_, func_, help_)                        \
+    SHELL_MODULE_CMD_WITH_NAME(mod_, cmd_, #cmd_, 1, func_, help_)
+
+/**
+ * @brief Register global shell command function
+ *
+ * Macro creates shell command that can be used when shell task is enabled
+ * This is equivalent of calling deprecated function shell_cmd_register.
+ *
+ * @name - cmd_name command name (it will be put in "" to make string
+ * @func - command function function of shell_cmd_ext_func_t
+ * @help pointer to shell_cmd_help
+ */
+#define MAKE_SHELL_CMD(cmd_name, func, help)                                  \
+    SHELL_MODULE_CMD(compat, cmd_name, func, help)
+
+/**
+ * @brief Register global extended shell command function
+ *
+ * Macro creates shell command that can be used when shell task is enabled
+ * This is equivalent of calling deprecated function shell_cmd_register.
+ *
+ * @name - cmd_name command name (it will be put in "" to make string
+ * @func - command function function of shell_cmd_ext_func_t
+ * @help pointer to shell_cmd_help
+ */
+#define MAKE_SHELL_EXT_CMD(cmd_name, func, help)                              \
+    SHELL_MODULE_EXT_CMD(compat, cmd_name, func, help)
 
 #if MYNEWT_VAL(SHELL_CMD_HELP)
 #define SHELL_HELP_(help_) (help_)
