01d53bac612f7b060c4966906386e7e2f9153c65
Jerzy Kasenberg <jerzy@apache.org>
2025-06-01 12:52:39 +0200

    test/i2c_scan: Register command statically
    Now i2c_scan shell command is register at link time to
    reduce memory footprint.
    No change in functionality.
    
    Signed-off-by: Jerzy Kasenberg <jerzy@apache.org>

--- test/i2c_scan/src/i2c_scan.c
+++ test/i2c_scan/src/i2c_scan.c
@@ -27,13 +27,6 @@
 #include <bus/drivers/i2c_common.h>
 #endif
 
-static int i2c_scan_cli_cmd(int argc, char **argv);
-
-static struct shell_cmd i2c_scan_cmd_struct = {
-    .sc_cmd = "i2c_scan",
-    .sc_cmd_func = i2c_scan_cli_cmd
-};
-
 #if MYNEWT_VAL(BUS_DRIVER_PRESENT)
 static int
 i2c_scan_probe(int i2c_num, uint16_t address, uint32_t timeout)
@@ -61,7 +54,8 @@ i2c_scan_probe(int i2cnum, uint16_t address, uint32_t timeout)
 #endif
 
 static int
-i2c_scan_cli_cmd(int argc, char **argv)
+i2c_scan_cli_cmd(const struct shell_cmd *cmd, int argc, char **argv,
+                 struct streamer *streamer)
 {
     uint8_t addr;
     int32_t timeout;
@@ -70,7 +64,7 @@ i2c_scan_cli_cmd(int argc, char **argv)
     int rc;
 
     if (argc < 2) {
-        console_printf("Specify i2c num\n");
+        streamer_printf(streamer, "Specify i2c num\n");
         return 0;
     }
 
@@ -79,37 +73,33 @@ i2c_scan_cli_cmd(int argc, char **argv)
     rc = 0;
     i2cnum = parse_ll_bounds(argv[1], 0, 0xf, &rc);
     if (rc) {
-        console_printf("Invalid i2c interface:%s\n", argv[2]);
+        streamer_printf(streamer, "Invalid i2c interface:%s\n", argv[2]);
         return 0;
     }
 
-    console_printf("Scanning I2C bus %u\n"
-                   "     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n"
-                   "00:                         ", i2cnum);
+    streamer_printf(streamer, "Scanning I2C bus %u\n"
+                    "     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n"
+                    "00:                         ", i2cnum);
 
     /* Scan all valid I2C addresses (0x08..0x77) */
     for (addr = 0x08; addr < 0x78; addr++) {
         rc = i2c_scan_probe(i2cnum, addr, timeout);
         /* Print addr header every 16 bytes */
         if (!(addr % 16)) {
-            console_printf("\n%02x: ", addr);
+            streamer_printf(streamer, "\n%02x: ", addr);
         }
         /* Display the addr if a response was received */
         if (!rc) {
-            console_printf("%02x ", addr);
+            streamer_printf(streamer, "%02x ", addr);
             dev_count++;
         } else {
-            console_printf("-- ");
+            streamer_printf(streamer, "-- ");
         }
         os_time_delay(OS_TICKS_PER_SEC / 1000 * 20);
     }
-    console_printf("\nFound %u devices on I2C bus %u\n", dev_count, i2cnum);
+    streamer_printf(streamer, "\nFound %u devices on I2C bus %u\n", dev_count, i2cnum);
 
     return 0;
 }
 
-void
-i2c_scan_init(void)
-{
-    shell_cmd_register(&i2c_scan_cmd_struct);
-}
+MAKE_SHELL_CMD(i2c_scan, i2c_scan_cli_cmd, NULL)
